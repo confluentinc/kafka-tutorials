cc79366e2f4f59f835ded482b22980eb7ce7d1cc
Michael Drogalis <michael.drogalis@confluent.io>
2019-03-31 18:37:20 -0700

    Exclude Gradle files from processing.

--- _includes/recipes/filtering/kstreams/code/src/main/java/io/confluent/developer/FilterEvents.java
+++ _includes/recipes/filtering/kstreams/code/src/main/java/io/confluent/developer/FilterEvents.java
@@ -1,11 +1,13 @@
 package io.confluent.developer;
+
+import io.confluent.developer.avro.User;
  
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
- 
+
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
  
@@ -13,21 +15,22 @@ public class FilterEvents {
  
     public static void main(String[] args) throws Exception {
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "filtering-app");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
  
         final StreamsBuilder builder = new StreamsBuilder();
  
-        builder.stream("streams-plaintext-input").to("streams-pipe-output");
+        builder.<Long, User>stream("user-events")
+            .filter((key, user) -> user.getFavoriteColor().equals("green"))
+            .to("filtered-user-events");
  
         final Topology topology = builder.build();
- 
         final KafkaStreams streams = new KafkaStreams(topology, props);
         final CountDownLatch latch = new CountDownLatch(1);
  
-        // attach shutdown handler to catch control-c
+        // Attach shutdown handler to catch Control-C.
         Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
             @Override
             public void run() {
