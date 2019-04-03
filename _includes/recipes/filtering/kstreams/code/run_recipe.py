b5e0c88abc80f3b7a8874b9c4cd89f1a89a65a20
Michael Drogalis <michael.drogalis@confluent.io>
2019-04-03 08:47:10 -0700

    Docker compose with better logging. Makefile builds recipe outputs.

---
+++ _includes/recipes/filtering/kstreams/code/run_recipe.py
@@ -0,0 +1,36 @@
+import subprocess
+import signal
+import os
+import sys
+
+if sys.version_info[0] != 3:
+    print("This script requires Python 3")
+    exit()
+
+producer_script = "recipe-steps/console-producer.sh"
+consumer_script = "recipe-steps/console-consumer.sh"
+run_app_script = "recipe-steps/run-dev-app.sh"
+
+inputs_file = "recipe-steps/input-events.json"
+outputs_file = "recipe-outputs/actual-output-events.json"
+
+timeout_ms = 3000
+
+producer = subprocess.run(["sh", producer_script],
+                          stdin=open(inputs_file, "r"),
+                          stdout=subprocess.PIPE)
+app = subprocess.Popen(["sh", run_app_script], preexec_fn=os.setsid)
+
+consumer_file = open(consumer_script, "r")
+consumer_cmd = consumer_file.read()[:-1] + " --timeout-ms " + str(timeout_ms)
+consumer_cmds = consumer_cmd.split()
+consumer_file.close()
+
+consumer = subprocess.run(consumer_cmds, stdout=subprocess.PIPE)
+
+os.killpg(os.getpgid(app.pid), signal.SIGTERM)
+app.terminate()
+
+output_file = open(outputs_file, "w")
+output_file.write(str(consumer.stdout, "UTF-8"))
+output_file.close()
