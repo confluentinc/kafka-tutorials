734520e31d4f42d1b824c95406cf23dc2295beb3
Michael Drogalis <michael.drogalis@confluent.io>
2019-04-04 11:28:40 -0700

    Support for copyable snippets.

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -8,4 +8,14 @@ $(document).ready(function() {
     $('#tab-content > .recipe-tab').removeClass('is-active');
     $('.recipe-tab[data-content="' + tab + '"]').addClass('is-active');
   });
+
+  var clipboard = new ClipboardJS('.copy-btn');
+
+  $('pre').each(function(index, element) {
+    var id = 'snippet-' + index;
+
+    $(element).wrap("<div class='snippet-wrapper'></div>");
+    $(element).attr('id', id);
+    $(element).after(`<button class='button is-small copy-btn' data-clipboard-target='#${id}'>Copy</button>`);
+  });
 });
