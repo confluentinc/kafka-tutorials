01474da7295b392faf33c21a7d97756485f33db6
Michael Drogalis <michael.drogalis@confluent.io>
2019-04-05 15:15:54 -0700

    Testing section.

--- _includes/recipes/filtering/kstreams/code/src/main/java/io/confluent/developer/FilterEvents.java
+++ _includes/recipes/filtering/kstreams/code/src/main/java/io/confluent/developer/FilterEvents.java
@@ -1,44 +1,63 @@
 package io.confluent.developer;
 
 import io.confluent.developer.avro.User;
-
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
-
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
 public class FilterEvents {
 
-    private final static String BOOTSTRAP_SERVERS = "127.0.0.1:29092";
-    private final static String SCHEMA_REGISTRY_URL = "http://127.0.0.1:8081";
+    public Properties getEnvProperties() throws IOException {
+        Properties props = new Properties();
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream is = classloader.getResourceAsStream("config.properties");
+        props.load(is);
+        is.close();
 
-    private final static String INPUT_TOPIC = "user-events";
-    private final static String OUTPUT_TOPIC = "filtered-user-events";
+        return props;
+    }
 
-    public static void main(String[] args) throws Exception {
+    public Properties buildStreamsProperties(Properties envProps) {
         Properties props = new Properties();
 
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "filtering-app");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, envProps.getProperty("application.id"));
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, envProps.getProperty("bootstrap.servers"));
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Long().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
-        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, SCHEMA_REGISTRY_URL);
+        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, envProps.getProperty("schema.registry.url"));
 
+        return props;
+    }
+
+    public Topology buildTopology(Properties envProps) {
         final StreamsBuilder builder = new StreamsBuilder();
 
-        builder.<Long, User>stream(INPUT_TOPIC)
-                .filter((key, user) -> user.getFavoriteColor().equals("green"))
-                .to(OUTPUT_TOPIC);
+        final String inputTopic = envProps.getProperty("input.topic");
+        final String outputTopic = envProps.getProperty("output.topic");
 
-        final Topology topology = builder.build();
-        final KafkaStreams streams = new KafkaStreams(topology, props);
+        builder.<Long, User>stream(inputTopic)
+                .filter((key, user) -> user.getFavoriteColor().equals("green"))
+                .to(outputTopic);
+
+        return builder.build();
+    }
+
+    public static void main(String[] args) throws Exception {
+        FilterEvents fe = new FilterEvents();
+        Properties envProps = fe.getEnvProperties();
+        Properties streamProps = fe.buildStreamsProperties(envProps);
+        Topology topology = fe.buildTopology(envProps);
+
+        final KafkaStreams streams = new KafkaStreams(topology, streamProps);
         final CountDownLatch latch = new CountDownLatch(1);
 
         // Attach shutdown handler to catch Control-C.
---
+++ _includes/recipes/filtering/kstreams/code/src/test/java/io/confluent/developer/FilterEventsTest.java
@@ -0,0 +1,113 @@
+package io.confluent.developer;
+
+import io.confluent.developer.avro.User;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroDeserializer;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerializer;
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.TopologyTestDriver;
+import org.apache.kafka.streams.test.ConsumerRecordFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+
+public class FilterEventsTest {
+
+    public SpecificAvroSerializer<User> makeSerializer(Properties envProps) {
+        SpecificAvroSerializer<User> serializer = new SpecificAvroSerializer<>();
+        serializer.configure(new HashMap<String, String>() {{
+            put("schema.registry.url", envProps.getProperty("schema.registry.url"));
+        }}, false);
+
+        return serializer;
+    }
+
+    public SpecificAvroDeserializer<User> makeDeserializer(Properties envProps) {
+        SpecificAvroDeserializer<User> deserializer = new SpecificAvroDeserializer<>();
+        deserializer.configure(new HashMap<String, String>() {{
+            put("schema.registry.url", envProps.getProperty("schema.registry.url"));
+        }}, false);
+
+        return deserializer;
+    }
+
+    @Test
+    public void testIt() throws IOException {
+        FilterEvents fe = new FilterEvents();
+        Properties envProps = fe.getEnvProperties();
+        Properties streamProps = fe.buildStreamsProperties(envProps);
+
+        String inputTopic = envProps.getProperty("input.topic");
+        String outputTopic = envProps.getProperty("output.topic");
+
+        Topology topology = fe.buildTopology(envProps);
+        TopologyTestDriver testDriver = new TopologyTestDriver(topology, streamProps);
+
+        Serializer<Long> keySerializer = Serdes.Long().serializer();
+        SpecificAvroSerializer<User> valueSerializer = makeSerializer(envProps);
+
+        Deserializer<Long> keyDeserializer = Serdes.Long().deserializer();
+        SpecificAvroDeserializer<User> valueDeserializer = makeDeserializer(envProps);
+
+        ConsumerRecordFactory<Long, User> inputFactory = new ConsumerRecordFactory<>(inputTopic, keySerializer, valueSerializer);
+
+        User michael = User.newBuilder().setName("michael").setFavoriteNumber(42).setFavoriteColor("green").build();
+        User tim = User.newBuilder().setName("tim").setFavoriteNumber(8).setFavoriteColor("green").build();
+        User jill = User.newBuilder().setName("jill").setFavoriteNumber(500).setFavoriteColor("red").build();
+        User lucas = User.newBuilder().setName("lucas").setFavoriteNumber(71).setFavoriteColor("blue").build();
+        User steve = User.newBuilder().setName("steve").setFavoriteNumber(23).setFavoriteColor("green").build();
+        User sally = User.newBuilder().setName("sally").setFavoriteNumber(63).setFavoriteColor("orange").build();
+        User john = User.newBuilder().setName("john").setFavoriteNumber(88).setFavoriteColor("green").build();
+        User fred = User.newBuilder().setName("fred").setFavoriteNumber(202).setFavoriteColor("blue").build();
+        User sue = User.newBuilder().setName("sue").setFavoriteNumber(0).setFavoriteColor("green").build();
+
+        List<User> input = new ArrayList<User>() {{
+            add(michael);
+            add(tim);
+            add(jill);
+            add(lucas);
+            add(steve);
+            add(sally);
+            add(john);
+            add(fred);
+            add(sue);
+        }};
+
+        List<User> expectedOutput = new ArrayList<User>() {{
+            add(michael);
+            add(tim);
+            add(steve);
+            add(john);
+            add(sue);
+        }};
+
+        for (User user : input) {
+            testDriver.pipeInput(inputFactory.create(0L, user));
+        }
+
+        List<User> actualOutput = new ArrayList<>();
+        while (true) {
+            ProducerRecord<Long, User> record = testDriver.readOutput(outputTopic, keyDeserializer, valueDeserializer);
+
+            if (record != null) {
+                actualOutput.add(record.value());
+            } else {
+                break;
+            }
+        }
+
+        Assert.assertEquals(expectedOutput, actualOutput);
+    }
+
+}
