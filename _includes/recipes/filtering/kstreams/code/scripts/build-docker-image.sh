153645de18c7d60ad2de56713b0391111f802454
Michael Drogalis <michael.drogalis@confluent.io>
2019-04-08 12:54:46 -0700

    Production portion of tutorial.

---
+++ _includes/recipes/filtering/kstreams/code/scripts/build-docker-image.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+cd "$(dirname "$0")/.."
+
+gradle -Penv=prod shadowJar
+docker build -t io.confluent.developer/kstreams-filter:0.0.1 .
