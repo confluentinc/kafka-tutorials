74fd11104ee171dbc833414267da537a5b13f2f7
Tim Berglund <tlberglund@gmail.com>
2019-04-12 16:25:12 +0900

    Some cleanup plus a suggested conversion to Docker Compose

--- _includes/recipes/filtering/kstreams/harness/recipe-steps/dev/console-producer.sh
+++ _includes/recipes/filtering/kstreams/harness/recipe-steps/dev/console-producer.sh
@@ -1,1 +1,1 @@
-docker exec -i schema-registry /usr/bin/kafka-avro-console-producer --topic user-events --broker-list broker:9092 --property value.schema="$(< src/main/avro/user.avsc)"
+docker-compose exec -i schema-registry /usr/bin/kafka-avro-console-producer --topic user-events --broker-list broker:9092 --property value.schema="$(< src/main/avro/user.avsc)"
