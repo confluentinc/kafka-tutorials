a1aecfd3cb5c1077c1705f8e4e08baefd2930c08
Hartanto Thio <hartanto@confluent.io>
2019-06-07 08:54:16 -0700

    initial commit for new design

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,26 +1,50 @@
 $(document).ready(function() {
-  var clipboard = new ClipboardJS('.copy-btn');
+  // Build the TOC
+  $("#toc").toc({
+    content: "div.recipe-markup",
+    headings: "h1,h2,h3,h4"
+  });
 
-  $('pre').each(function(index, element) {
+  // Code highlight
+  hljs.initHighlightingOnLoad();
+
+  var clipboard = new ClipboardJS(".copy-btn");
+
+  $("pre").each(function(index, element) {
     //Set up copy buttons.
-    var id = 'snippet-' + index;
+    var id = "snippet-" + index;
 
     $(element).wrap("<div class='snippet-wrapper'></div>");
-    $(element).attr('id', id);
-    $(element).after(`<button class='button is-small code-btn copy-btn' data-clipboard-target='#${id}'><span class="icon"><i class="far fa-copy"></i></span></button>`);
+    $(element).attr("id", id);
+    $(element).after(
+      `<button class='button is-small code-btn copy-btn' data-clipboard-target='#${id}'><span class="icon"><i class="far fa-copy"></i></span></button>`
+    );
 
     //Set up expand buttons.
     var maxHeight = 320;
     var actualHeight = $(element).height();
 
     if (actualHeight > maxHeight) {
-      $(element).css('max-height', maxHeight);
-      $(element).after("<button class='button is-small code-btn expand-btn'>Expand</button>");
+      $(element).css("max-height", maxHeight);
+      $(element).after(
+        "<button class='button is-small code-btn expand-btn'>Expand</button>"
+      );
     }
   });
 
-  $('.expand-btn').click(function() {
-    $(this).prev('pre').css('max-height', '');
+  $(".expand-btn").click(function() {
+    $(this)
+      .prev("pre")
+      .css("max-height", "");
     $(this).remove();
+  });
+
+  $(".num").each(function() {
+    var text = $(this).text();
+    $(this).html(text.substring(0, text.length - 1));
+  });
+
+  $(".cflt-options select").on("change", function() {
+    alert(this.value);
   });
 });
