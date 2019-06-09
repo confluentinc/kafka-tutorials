12167ef6ab490d925486e211e9fd0bb799333736
Tim Berglund <tlberglund@gmail.com>
2019-06-08 18:02:35 -0600

    Finished 'take it to prod' procedure

--- _includes/recipes/transforming/kstreams/harness/recipe-steps/prod/build-image.sh
+++ _includes/recipes/transforming/kstreams/harness/recipe-steps/prod/build-image.sh
@@ -1,1 +1,1 @@
-gradle jibDockerBuild --image=io.confluent.developer/kstreams-split:0.0.1
+gradle jibDockerBuild --image=io.confluent.developer/kstreams-transform:0.0.1
--- _includes/recipes/transforming/kstreams/harness/recipe-steps/prod/launch-container.sh
+++ _includes/recipes/transforming/kstreams/harness/recipe-steps/prod/launch-container.sh
@@ -1,1 +1,1 @@
-docker run -v $PWD/configuration/prod.properties:/config.properties io.confluent.developer/kstreams-split:0.0.1 config.properties 
+docker run -v $PWD/configuration/prod.properties:/config.properties io.confluent.developer/kstreams-transform:0.0.1 config.properties 
