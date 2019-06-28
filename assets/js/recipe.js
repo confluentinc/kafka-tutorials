b169b5f3970f8fa3652b471dd8163fc6d906b55c
Hartanto Thio <hartanto@confluent.io>
2019-06-28 09:17:38 -0700

    add compress button

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  var CODE_BLOCK_HEIGHT = 320;
+
   // Build the TOC
   $("#toc").toc({
     content: "div.recipe-markup",
@@ -13,35 +15,47 @@ $(document).ready(function() {
   $("pre").each(function(index, element) {
     //Set up copy buttons.
     var id = "snippet-" + index;
+    var $element = $(element);
 
-    $(element).wrap("<div class='snippet-wrapper'></div>");
-    $(element).attr("id", id);
-    $(element).after(
+    $element.wrap("<div class='snippet-wrapper'></div>");
+    $element.attr("id", id);
+    $element.after(
       `<span class="icon copy-btn" data-clipboard-target='#${id}'><i class="far fa-copy"></i></span>`
     );
 
     //Set up expand buttons.
-    var maxHeight = 320;
-    var actualHeight = $(element).height();
+    var actualHeight = $element.height();
 
-    if (actualHeight > maxHeight) {
-      $(element).css("max-height", maxHeight);
-      $(element).after(
-        "<span class='icon expand-btn'><i class='fas fa-expand-arrows-alt'></i></span>"
+    if (actualHeight > CODE_BLOCK_HEIGHT) {
+      $element.css("max-height", CODE_BLOCK_HEIGHT);
+      $element.after(
+        "<span class='icon toggle-expand expand-btn'><i class='fas fa-expand-arrows-alt'></i></span><span class='icon toggle-expand compress-btn is-hidden'><i class='fas fa-compress-arrows-alt'></i></span>"
       );
     }
   });
 
-  $(".expand-btn").click(function() {
-    $(this)
-      .prev("pre")
-      .css("max-height", "");
-    $(this).remove();
+  $('.expand-btn').click(function() {
+      $(this)
+        .siblings("pre")
+        .css("max-height", "");
+
+      this.classList.add('is-hidden');
+      $('.compress-btn').removeClass('is-hidden');
+  });
+
+  $('.compress-btn').click(function() {
+      $(this)
+        .siblings("pre")
+        .css("max-height", CODE_BLOCK_HEIGHT);
+
+      this.classList.add('is-hidden');
+      $('.expand-btn').removeClass('is-hidden');
   });
 
   $(".num").each(function() {
-    var text = $(this).text();
-    $(this).html(text.substring(0, text.length - 1));
+    var $this = $(this);
+    var text = $this.text();
+    $this.html(text.substring(0, text.length - 1));
   });
 
   $(".cflt-options select").on("change", function() {
