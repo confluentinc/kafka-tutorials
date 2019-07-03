8f6f1f3c330e62958442892c768c04b079bc4f2b
riferrei <riferrei@riferrei.net>
2019-07-02 20:18:56 -0400

    Read proof the HTML content for better user experience

--- _includes/recipes/transforming/kafka/harness/recipe-steps/dev/console-producer.sh
+++ _includes/recipes/transforming/kafka/harness/recipe-steps/dev/console-producer.sh
@@ -1,1 +1,1 @@
-docker exec -i schema-registry /usr/bin/kafka-avro-console-producer --topic raw-movies --broker-list broker:9092 --property value.schema="$(< src/main/avro/input_movie_event.avsc)"
+docker exec -i schema-registry /usr/bin/kafka-avro-console-producer --topic raw-movies --broker-list broker:9092 --property value.schema="$(< src/main/avro/input-movie-event.avsc)"
