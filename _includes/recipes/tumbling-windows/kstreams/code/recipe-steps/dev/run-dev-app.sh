36d444e777dd5030cf4cb9079a6f4e983cbecfda
Tim Berglund <tlberglund@gmail.com>
2019-07-08 15:44:36 +0100

    Dev harness runs to completion

--- _includes/recipes/tumbling-windows/kstreams/code/recipe-steps/dev/run-dev-app.sh
+++ _includes/recipes/tumbling-windows/kstreams/code/recipe-steps/dev/run-dev-app.sh
@@ -1,1 +1,1 @@
-java -jar build/libs/kstreams-tumbling-windows-0.0.1.jar configuration/dev.properties
+java -jar build/libs/kstreams-tumbling-windows-standalone-0.0.1.jar configuration/dev.properties
