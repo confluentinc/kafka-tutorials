6a72c99d7c1b2bffa47cf72c6b21622bbd396f47
Hartanto Thio <hartanto@confluent.io>
2019-07-12 15:48:53 -0700

    Added marketo form

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+$(function() {
+    var MKTO_FORM_ID = 3977;
+    var theForm;
+
+    window.MktoForms2.loadForm("//go.confluent.io", "582-QHX-262", MKTO_FORM_ID, function(form) {
+        theForm = form;
+
+        form.onSuccess(function(vals, thanksURL) {
+            var $mktoForm = $('#mkto-form .form');
+
+            $mktoForm.children(':not(.is-hidden)').hide();
+            $mktoForm.children('.is-hidden').removeClass('is-hidden');
+
+            return false;
+        });
+    });
+
+    $('#mkto-form a').on('click', function() {
+        $(this).css('z-index', 0).siblings('form').addClass('active').find('input').focus();
+
+        return false;
+    });
+
+    $('#mkto-form form').on('submit', function() {
+        var vals = {};
+
+        // Update the real marketo form
+        $(this).serializeArray().forEach(function(field) {
+            vals[field.name] = field.value;
+        });
+
+        theForm.setValues(vals);
+
+        // Submit marketo form
+        theForm.submit();
+
+        return false;
+    });
+});
