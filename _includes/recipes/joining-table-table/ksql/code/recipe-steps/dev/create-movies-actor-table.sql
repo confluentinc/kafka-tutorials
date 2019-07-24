d5473881997bcc9d53cc473ad24fc683a00258fd
Michael Drogalis <michael.drogalis@confluent.io>
2019-07-23 17:27:26 -0700

    Some aesthetic fixes.

--- _includes/recipes/joining-table-table/ksql/code/recipe-steps/dev/create-movies-actor-table.sql
+++ _includes/recipes/joining-table-table/ksql/code/recipe-steps/dev/create-movies-actor-table.sql
@@ -1,4 +1,4 @@
 CREATE TABLE lead_actor (title VARCHAR, actor_name VARCHAR) 
              WITH (KAFKA_TOPIC='lead_actors', 
                    PARTITIONS=1, 
-                   VALUE_FORMAT='AVRO');
+                   VALUE_FORMAT='avro');
--- _includes/recipes/joining-table-table/ksql/code/recipe-steps/dev/create-movies-table.sql
+++ _includes/recipes/joining-table-table/ksql/code/recipe-steps/dev/create-movies-table.sql
@@ -1,4 +1,4 @@
 CREATE TABLE movies (id INT, title VARCHAR, release_year INT)
              WITH (KAFKA_TOPIC='movies', 
                    PARTITIONS=1, 
-                   VALUE_FORMAT='Avro');
+                   VALUE_FORMAT='avro');
