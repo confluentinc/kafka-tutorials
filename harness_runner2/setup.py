6dd39c9d343962bad32eebd22571f8cdc41676c2
Michael Drogalis <michael.drogalis@confluent.io>
2019-07-27 14:35:39 -0700

    harness2 -> 1

--- harness_runner2/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='harness-runner2',
-    version='0.0.1',
-    scripts=['harness-runner2']
-)
--- harness_runner2/util.py
+++
@@ -1,2 +0,0 @@
-def in_base_dir(context, f):
-    return context["base_dir"] + "/" + f
