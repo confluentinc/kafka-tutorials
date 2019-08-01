495629930e75fff2c0cbab4278f3ac0641585b69
Robin Moffatt <robin@confluent.io>
2019-08-01 17:10:13 +0100

    Rework session window recipe into new adoc format

--- _includes/recipes/session-windows/ksql/code/recipe-steps/test/run-tests.sh
+++ _includes/recipes/session-windows/ksql/code/recipe-steps/test/run-tests.sh
@@ -1,1 +1,3 @@
-docker exec ksql-cli ksql-test-runner -i /opt/app/test/input.json -s opt/app/src/statements.sql -o /opt/app/test/output.json
+#!/usr/bin/env bash
+
+docker exec ksql-cli ksql-test-runner -i /opt/app/test/input.json -s /opt/app/src/statements.sql -o /opt/app/test/output.json
