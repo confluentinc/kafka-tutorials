cc05a981143d5832237f35f853fc30f1bd7b66e5
Yeva Byzek <yeva@confluent.io>
2019-08-07 19:25:08 -0400

    Materialized

---
+++ _includes/tutorials/aggregating-sum/kstreams/code/tutorial-steps/dev/run-dev-app.sh
@@ -0,0 +1,1 @@
+java -jar build/libs/kstreams-filter-standalone-0.0.1.jar configuration/dev.properties
---
+++ _includes/tutorials/aggregating-sum/kstreams/code/tutorial-steps/prod/launch-container.sh
@@ -0,0 +1,1 @@
+docker run -v $PWD/configuration/prod.properties:/config.properties io.confluent.developer/kstreams-aggregating-sum:0.0.1 config.properties
