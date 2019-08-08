79c2c24d3812c1263f5de57eaec67b7f9489f0df
Yeva Byzek <yeva@confluent.io>
2019-08-08 00:30:41 -0400

    Fix port number on brokers

--- _includes/tutorials/aggregating-sum/kstreams/code/tutorial-steps/dev/console-consumer.sh
+++ _includes/tutorials/aggregating-sum/kstreams/code/tutorial-steps/dev/console-consumer.sh
@@ -1,1 +1,1 @@
-docker exec -it schema-registry /usr/bin/kafka-avro-console-consumer --topic MOVIE_REVENUE --bootstrap-server broker:29092 --from-beginning
+docker exec -it broker /usr/bin/kafka-console-consumer --topic MOVIE_REVENUE --bootstrap-server broker:29092 --from-beginning --property print.key=true
--- _includes/tutorials/aggregating-sum/kstreams/code/tutorial-steps/dev/console-producer.sh
+++ _includes/tutorials/aggregating-sum/kstreams/code/tutorial-steps/dev/console-producer.sh
@@ -1,1 +1,1 @@
-docker exec -i schema-registry /usr/bin/kafka-avro-console-producer --topic movie-ticket-sales --broker-list broker:9092 --property value.schema="$(< src/main/avro/ticketsale.avsc)"
+docker exec -i schema-registry /usr/bin/kafka-avro-console-producer --topic movie-ticket-sales --broker-list broker:29092 --property value.schema="$(< src/main/avro/ticketsale.avsc)"
