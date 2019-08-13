4508db0838fff5df98a63224518043fff3798d77
Michael Drogalis <michael.drogalis@confluent.io>
2019-08-13 09:15:52 -0700

    Conforms SQL format to other examples.

--- _includes/tutorials/joining-stream-stream/ksql/code/recipe-steps/dev/create-orders-stream.sql
+++ _includes/tutorials/joining-stream-stream/ksql/code/recipe-steps/dev/create-orders-stream.sql
@@ -1,8 +1,6 @@
 CREATE STREAM ORDERS (ORDER_ID INT, ORDER_TS VARCHAR, TOTAL_AMOUNT DOUBLE, CUSTOMER_NAME VARCHAR)
-              WITH (KAFKA_TOPIC='_orders',
-                    VALUE_FORMAT='JSON',
-                    TIMESTAMP='ORDER_TS',
-                    TIMESTAMP_FORMAT='yyyy-MM-dd''T''HH:mm:ssX',
-                    PARTITIONS=4,
-                    REPLICAS=1);
-                    
+    WITH (KAFKA_TOPIC='_orders',
+          VALUE_FORMAT='JSON',
+          TIMESTAMP='ORDER_TS',
+          TIMESTAMP_FORMAT='yyyy-MM-dd''T''HH:mm:ssX',
+          PARTITIONS=4);
--- _includes/tutorials/joining-stream-stream/ksql/code/recipe-steps/dev/create-shipments-stream.sql
+++ _includes/tutorials/joining-stream-stream/ksql/code/recipe-steps/dev/create-shipments-stream.sql
@@ -1,7 +1,6 @@
 CREATE STREAM SHIPMENTS (SHIPMENT_ID VARCHAR, SHIP_TS VARCHAR, ORDER_ID INT, WAREHOUSE VARCHAR)
-              WITH (KAFKA_TOPIC='_shipments',
-                    VALUE_FORMAT='JSON',
-                    TIMESTAMP='SHIP_TS',
-                    TIMESTAMP_FORMAT='yyyy-MM-dd''T''HH:mm:ssX',
-                    PARTITIONS=4,
-                    REPLICAS=1);
+    WITH (KAFKA_TOPIC='_shipments',
+          VALUE_FORMAT='JSON',
+          TIMESTAMP='SHIP_TS',
+          TIMESTAMP_FORMAT='yyyy-MM-dd''T''HH:mm:ssX',
+          PARTITIONS=4);
