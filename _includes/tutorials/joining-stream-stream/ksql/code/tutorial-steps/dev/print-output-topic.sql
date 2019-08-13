990c7decc0fa0694e0e72d3a04466afd1e25e95a
riferrei <riferrei@riferrei.net>
2019-08-13 17:29:13 -0400

    Changed print output from query to print stmt

--- _includes/tutorials/joining-stream-stream/ksql/code/tutorial-steps/dev/print-output-topic.sql
+++ _includes/tutorials/joining-stream-stream/ksql/code/tutorial-steps/dev/print-output-topic.sql
@@ -1,5 +1,1 @@
-SELECT
-    order_id, order_ts, total_amount, customer_name,
-    shipment_id, shipment_ts, warehouse, ship_time
-FROM SHIPPED_ORDERS
-LIMIT 3;
+PRINT SHIPPED_ORDERS FROM BEGINNING LIMIT 3;
