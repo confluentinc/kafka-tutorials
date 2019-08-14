f8caf3c71c4a9cd14f6e8ef109a083faa83671eb
Michael Drogalis <michael.drogalis@confluent.io>
2019-08-13 17:38:08 -0700

    Some copy editing.

--- _includes/tutorials/joining-stream-stream/ksql/code/tutorial-steps/dev/continuous-join.sql
+++ _includes/tutorials/joining-stream-stream/ksql/code/tutorial-steps/dev/continuous-join.sql
@@ -1,11 +1,11 @@
-CREATE STREAM SHIPPED_ORDERS AS
-    SELECT O.ORDER_ID AS ORDER_ID,
-           TIMESTAMPTOSTRING(O.ROWTIME, 'yyyy-MM-dd HH:mm:ss') AS ORDER_TS,
-           O.TOTAL_AMOUNT,
-           O.CUSTOMER_NAME,
-           S.SHIPMENT_ID,
-           TIMESTAMPTOSTRING(S.ROWTIME, 'yyyy-MM-dd HH:mm:ss') AS SHIPMENT_TS,
-           S.WAREHOUSE, (S.ROWTIME - O.ROWTIME) / 1000 / 60 AS SHIP_TIME
-    FROM ORDERS O INNER JOIN SHIPMENTS S
+CREATE STREAM shipped_orders AS
+    SELECT o.order_id AS order_id,
+           TIMESTAMPTOSTRING(o.rowtime, 'yyyy-MM-dd HH:mm:ss') AS order_ts,
+           o.total_amount,
+           o.customer_name,
+           s.shipment_id,
+           TIMESTAMPTOSTRING(s.rowtime, 'yyyy-MM-dd HH:mm:ss') AS shipment_ts,
+           s.warehouse, (s.rowtime - o.rowtime) / 1000 / 60 AS ship_time
+    FROM orders o INNER JOIN shipments s
     WITHIN 7 DAYS
-    ON O.ORDER_ID = S.ORDER_ID;
+    ON o.order_id = s.order_id;
