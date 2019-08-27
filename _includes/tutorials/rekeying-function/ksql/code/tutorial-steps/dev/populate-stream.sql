9044c8a0bab7b11fcd6809cccaa8c3db745a4dee
Russ Sayers <russell.sayers@gmail.com>
2019-08-27 15:01:29 -0700

    initial commit: Rekey a stream with a function (#126)
    * initial commit: Rekey a stream with a function
    
    * Rekey a stream with a function:
    
    * Added checks/unittests for null in the UDF parameters
    
    * Adding Michael Drogalis' feedback
    
    * Rekey a stream with a function: updates from the pull request feedback

---
+++ _includes/tutorials/rekeying-function/ksql/code/tutorial-steps/dev/populate-stream.sql
@@ -0,0 +1,10 @@
+INSERT INTO customers (id, firstname, lastname, phonenumber) VALUES (1, 'Sleve', 'McDichael', '(360) 555-8909');
+INSERT INTO customers (id, firstname, lastname, phonenumber) VALUES (2, 'Onson', 'Sweemey', '206-555-1272');
+INSERT INTO customers (id, firstname, lastname, phonenumber) VALUES (3, 'Darryl', 'Archideld', '425.555.6940');
+INSERT INTO customers (id, firstname, lastname, phonenumber) VALUES (4, 'Anatoli', 'Smorin', '509.555.8033');
+INSERT INTO customers (id, firstname, lastname, phonenumber) VALUES (5, 'Rey', 'McSriff', '360 555 6952');
+INSERT INTO customers (id, firstname, lastname, phonenumber) VALUES (6, 'Glenallen', 'Mixon', '(253) 555-7050');
+INSERT INTO customers (id, firstname, lastname, phonenumber) VALUES (7, 'Mario', 'McRlwain', '360 555 7598');
+INSERT INTO customers (id, firstname, lastname, phonenumber) VALUES (8, 'Kevin', 'Nogilny', '206.555.8090');
+INSERT INTO customers (id, firstname, lastname, phonenumber) VALUES (9, 'Tony', 'Smehrik', '425-555-7926');
+INSERT INTO customers (id, firstname, lastname, phonenumber) VALUES (10, 'Bobson', 'Dugnutt', '509.555.8795');
