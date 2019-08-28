73d2b33de9de33e3fe1783bfbb30a344c3c05ac7
Yeva Byzek <yeva@confluent.io>
2019-08-28 08:46:15 -0400

    Add console consumer for cities and cities_keyed

--- _includes/tutorials/connect-jdbc-specificavro/kstreams/code/tutorial-steps/dev/console-consumer.sh
+++ _includes/tutorials/connect-jdbc-specificavro/kstreams/code/tutorial-steps/dev/console-consumer.sh
@@ -1,1 +1,3 @@
-docker exec -it schema-registry /usr/bin/kafka-avro-console-consumer --topic cities --bootstrap-server broker:9092 --from-beginning --property schema.registry.url=localhost:8082
+docker exec -it schema-registry /usr/bin/kafka-avro-console-consumer --topic cities --bootstrap-server broker:29092 --from-beginning --property schema.registry.url=http://localhost:8081 --property print.key=true --property key.deserializer=org.apache.kafka.common.serialization.LongDeserializer
+
+docker exec -it schema-registry /usr/bin/kafka-avro-console-consumer --topic cities_keyed --bootstrap-server broker:29092 --from-beginning --property schema.registry.url=http://localhost:8081 --property print.key=true --property key.deserializer=org.apache.kafka.common.serialization.LongDeserializer
