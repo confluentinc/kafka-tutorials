535400bf6614a612d508473c51ac8f8b26743a6e
Yeva Byzek <ybyzek@users.noreply.github.com>
2020-01-06 08:01:33 -0500

    DEVX-1372: Aggregations: Count (KStreams) (#168)

---
+++ _includes/tutorials/aggregating-count/kstreams/code/tutorial-steps/dev/run-dev-app.sh
@@ -0,0 +1,1 @@
+java -jar build/libs/kstreams-aggregating-count-standalone-0.0.1.jar configuration/dev.properties
---
+++ _includes/tutorials/aggregating-count/kstreams/code/tutorial-steps/prod/build-image.sh
@@ -0,0 +1,1 @@
+gradle jibDockerBuild --image=io.confluent.developer/kstreams-aggregating-count:0.0.1
