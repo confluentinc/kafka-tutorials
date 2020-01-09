dc006d05ed721be91a5ddaa7048bf4e5f8867553
Yeva Byzek <ybyzek@users.noreply.github.com>
2020-01-09 10:41:17 -0500

    DEVX-1039 for GH #123: Create a KStream: JDBC source connector -> Key:null and Value:SpecificAvro (#131)

---
+++ _includes/tutorials/connect-jdbc-specificavro/kstreams/code/tutorial-steps/dev/run-connector.sh
@@ -0,0 +1,1 @@
+curl -X POST -H Accept:application/json -H Content-Type:application/json http://localhost:8083/connectors/ -d @jdbc_source.config
---
+++ _includes/tutorials/connect-jdbc-specificavro/kstreams/code/tutorial-steps/dev/view-source-table.sh
@@ -0,0 +1,1 @@
+echo 'select * from cities;' | docker exec -i sqlitekt sqlite3 geos.db 
