3015f131905c18e6fb58687f9af48c8fb9df78d8
Colin Hicks <colin.hicks@gmail.com>
2020-01-09 13:36:30 -0500

    Revert "Filtering using Headers (Kafka) (#117)"
    This reverts commit 09d83eb35f0bb2bcce2f38bfeb2e21dce0887493.

--- _includes/tutorials/filtering-headers/kafka/code/tutorial-steps/dev/console-producer.sh
+++
@@ -1,1 +0,0 @@
-java -cp build/libs/kafka-filtering-headers-standalone-0.0.1.jar io.confluent.developer.ConsoleProducer configuration/dev.properties 
--- _includes/tutorials/filtering-headers/kafka/code/tutorial-steps/dev/run-dev-app.sh
+++
@@ -1,1 +0,0 @@
-java -jar build/libs/kafka-filtering-headers-standalone-0.0.1.jar configuration/dev.properties
