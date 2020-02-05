d1a56329ceb4584208aa76517526cf169c9bc22b
Ricardo Ferreira <riferrei@riferrei.net>
2020-02-05 11:50:24 -0500

    Find Distinct Events in a Stream (KSQL) (#187)
    * ksql-finding-distinct-first-commit
    
    * Update _includes/tutorials/finding-distinct/ksql/markup/dev/create-inputs.adoc
    
    Co-Authored-By: Colin Hicks <colin.hicks@confluent.io>
    
    * Update _includes/tutorials/finding-distinct/ksql/markup/dev/set-properties.adoc
    
    Co-Authored-By: Colin Hicks <colin.hicks@confluent.io>
    
    * Update _includes/tutorials/finding-distinct/ksql/markup/dev/transient-query.adoc
    
    Co-Authored-By: Colin Hicks <colin.hicks@confluent.io>
    
    * Update _includes/tutorials/finding-distinct/ksql/markup/dev/transient-window.adoc
    
    Co-Authored-By: Colin Hicks <colin.hicks@confluent.io>
    
    * Update _data/tutorials.yaml
    
    Co-Authored-By: Colin Hicks <colin.hicks@confluent.io>
    
    * Including flatten nested data in semaphore
    
    Co-authored-by: Colin Hicks <colin.hicks@gmail.com>

---
+++ _includes/tutorials/finding-distinct/ksql/code/tutorial-steps/dev/populate-stream.sql
@@ -0,0 +1,7 @@
+INSERT INTO CLICKS (IP_ADDRESS, URL, TIMESTAMP) VALUES ('10.0.0.1', 'https://docs.confluent.io/current/tutorials/examples/kubernetes/gke-base/docs/index.html', '2020-01-17T14:50:43+00:00');
+INSERT INTO CLICKS (IP_ADDRESS, URL, TIMESTAMP) VALUES ('10.0.0.2', 'https://www.confluent.io/hub/confluentinc/kafka-connect-datagen', '2020-01-17T14:53:44+00:01');
+INSERT INTO CLICKS (IP_ADDRESS, URL, TIMESTAMP) VALUES ('10.0.0.3', 'https://www.confluent.io/hub/confluentinc/kafka-connect-datagen', '2020-01-17T14:56:45+00:03');
+
+INSERT INTO CLICKS (IP_ADDRESS, URL, TIMESTAMP) VALUES ('10.0.0.1', 'https://docs.confluent.io/current/tutorials/examples/kubernetes/gke-base/docs/index.html', '2020-01-17T14:50:43+00:00');
+INSERT INTO CLICKS (IP_ADDRESS, URL, TIMESTAMP) VALUES ('10.0.0.2', 'https://www.confluent.io/hub/confluentinc/kafka-connect-datagen', '2020-01-17T14:53:44+00:01');
+INSERT INTO CLICKS (IP_ADDRESS, URL, TIMESTAMP) VALUES ('10.0.0.3', 'https://www.confluent.io/hub/confluentinc/kafka-connect-datagen', '2020-01-17T14:56:45+00:03');
