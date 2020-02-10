424c65e4ca88513e5f79d02ccac90d6f7cae8457
Bill Bejeck <bill@confluent.io>
2020-02-10 17:11:56 -0500

    Increase pause to harness for results, add close timeout back to streams, remove spaces from expected output results

--- _includes/tutorials/fk-joins/kstreams/code/src/main/java/io/confluent/developer/FkJoinTableToTable.java
+++ _includes/tutorials/fk-joins/kstreams/code/src/main/java/io/confluent/developer/FkJoinTableToTable.java
@@ -145,7 +145,7 @@ public class FkJoinTableToTable {
         Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
             @Override
             public void run() {
-                streams.close();
+                streams.close(Duration.ofSeconds(5));
                 latch.countDown();
             }
         });
