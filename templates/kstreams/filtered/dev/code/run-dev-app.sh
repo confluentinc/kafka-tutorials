0c430fb0023d6780ef2f6ee3f41bc72113b6bd91
Bill Bejeck <bill@confluent.io>
2020-02-27 18:12:07 -0500

    Initial work on tool chain improvements

---
+++ templates/kstreams/filtered/dev/code/run-dev-app.sh
@@ -0,0 +1,1 @@
+java -jar build/libs/<TUTORIAL-SHORT-NAME>-standalone-0.0.1.jar configuration/dev.properties
---
+++ templates/kstreams/static/dev/code/console-consumer.sh
@@ -0,0 +1,1 @@
+docker exec -it schema-registry /usr/bin/kafka-avro-console-consumer --topic music-interest --bootstrap-server broker:9092 --from-beginning
