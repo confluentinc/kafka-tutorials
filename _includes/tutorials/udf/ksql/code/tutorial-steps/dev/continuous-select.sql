ca17ee76be8491de5fb419f51af51abdc9451bb9
Colin Hicks <colin.hicks@confluent.io>
2020-03-02 18:54:13 -0500

    Adapt udf tutorial for ksqlDB 0.7.1 (#232)

--- _includes/tutorials/udf/ksql/code/tutorial-steps/dev/continuous-select.sql
+++ _includes/tutorials/udf/ksql/code/tutorial-steps/dev/continuous-select.sql
@@ -1,4 +1,5 @@
 CREATE STREAM vwap WITH (kafka_topic = 'vwap', partitions = 1) AS
     SELECT ticker, 
            vwap(bid, bidqty, ask, askqty) AS vwap
-    FROM raw_quotes;
+    FROM raw_quotes
+    EMIT CHANGES;
