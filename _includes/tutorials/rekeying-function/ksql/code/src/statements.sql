4d31e9b78679f7db3659f94fcf359e2dfd64a82b
Colin Hicks <colin.hicks@gmail.com>
2020-03-02 14:24:34 -0500

    Minor; capitalization consistency

--- _includes/tutorials/rekeying-function/ksql/code/src/statements.sql
+++ _includes/tutorials/rekeying-function/ksql/code/src/statements.sql
@@ -1,4 +1,4 @@
-CREATE STREAM customers (ROWKEY int KEY, firstname string, lastname string, phonenumber string)
+CREATE STREAM customers (rowkey int key, firstname string, lastname string, phonenumber string)
   WITH (kafka_topic='customers',
         partitions=2,
         value_format = 'avro');
