382873a4c0db4ffd3a21bdd2c5e363aee95c51ec
Colin Hicks <colin.hicks@confluent.io>
2020-03-03 15:53:27 -0500

    Updates serialization tutorial for ksqlDB 0.7.1 (#234)

--- _includes/tutorials/serialization/ksql/code/tutorial-steps/dev/create-avro-movies.sql
+++ _includes/tutorials/serialization/ksql/code/tutorial-steps/dev/create-avro-movies.sql
@@ -1,3 +1,7 @@
 CREATE STREAM movies_avro
     WITH (KAFKA_TOPIC='avro-movies', VALUE_FORMAT='avro') AS
-    SELECT * FROM movies_json;
+    SELECT
+        ROWKEY as MOVIE_ID,
+        TITLE,
+        RELEASE_YEAR
+    FROM movies_json;
--- _includes/tutorials/serialization/ksql/code/tutorial-steps/dev/create-json-movies.sql
+++ _includes/tutorials/serialization/ksql/code/tutorial-steps/dev/create-json-movies.sql
@@ -1,4 +1,4 @@
-CREATE STREAM movies_json (movie_id BIGINT, title VARCHAR, release_year INT)
+CREATE STREAM movies_json (ROWKEY BIGINT KEY, title VARCHAR, release_year INT)
     WITH (KAFKA_TOPIC='json-movies',
           PARTITIONS=1,
           VALUE_FORMAT='json');
