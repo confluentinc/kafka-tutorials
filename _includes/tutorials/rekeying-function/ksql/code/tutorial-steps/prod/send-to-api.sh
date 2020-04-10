3cbd02d51a5ec2e64359b9b9699d65ec5f31690f
Russ Sayers <russell.sayers@gmail.com>
2020-04-10 13:17:24 -0700

    Fixes for escaping json in the  "statements.sql"

--- _includes/tutorials/rekeying-function/ksql/code/tutorial-steps/prod/send-to-api.sh
+++ _includes/tutorials/rekeying-function/ksql/code/tutorial-steps/prod/send-to-api.sh
@@ -1,7 +1,7 @@
 tr '\n' ' ' < src/statements.sql | \
 sed 's/;/;\'$'\n''/g' | \
 while read stmt; do
-    echo '{"ksql":"'$stmt'", "streamsProperties": {}}' | \
+    jq --arg value1 "$stmt" -n '{ "ksql" : $value1 }'| \
         curl -s -X "POST" "http://localhost:8088/ksql" \
              -H "Content-Type: application/vnd.ksql.v1+json; charset=utf-8" \
              -d @- | \
