178db582e4218160d200deac7a57767dfc77c6d7
Andy Coates <big-andy-coates@users.noreply.github.com>
2020-04-30 15:18:03 +0100

    bump

--- _includes/tutorials/joining-table-table/ksql/code/src/statements.sql
+++ _includes/tutorials/joining-table-table/ksql/code/src/statements.sql
@@ -3,6 +3,7 @@ CREATE TABLE movies (ROWKEY VARCHAR KEY, id INT, title VARCHAR, release_year INT
                    PARTITIONS=1, 
                    VALUE_FORMAT='avro');
 
+
 CREATE TABLE lead_actor (ROWKEY VARCHAR KEY, title VARCHAR, actor_name VARCHAR)
              WITH (KAFKA_TOPIC='lead_actors', 
                    PARTITIONS=1, 
