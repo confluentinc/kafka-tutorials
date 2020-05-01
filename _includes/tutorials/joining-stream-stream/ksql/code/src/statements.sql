6aab2b763a3643865b3173c00df01f180e08773a
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2020-05-01 17:54:48 +0100

    Updated KSQL tutorials - 0.8.x (#337)
    Tested KSQL joins tutorials against 0.8.1 and found some issues, which are fixed below:
    
    - PRINT command no longer needs the topic name quoting: it's now case sensitive. (fixed across all examples)
    - TIMESTAMPTOSTRING should be passed a timezone for consistent results
    - Added explicit ROWKEY columns to all CT/CS statements, as this is to be encouraged.
    - Fixed key format output of PRINT that was outputting MIXED, which is not a valid output.
    
    fixes #336
    
    
    Co-authored-by: Andy Coates <big-andy-coates@users.noreply.github.com>

--- _includes/tutorials/joining-stream-stream/ksql/code/src/statements.sql
+++ _includes/tutorials/joining-stream-stream/ksql/code/src/statements.sql
@@ -5,7 +5,7 @@ CREATE STREAM orders (ROWKEY INT KEY, order_ts VARCHAR, total_amount DOUBLE, cus
           TIMESTAMP_FORMAT='yyyy-MM-dd''T''HH:mm:ssX',
           PARTITIONS=4);
 
-CREATE STREAM shipments (shipment_id VARCHAR, ship_ts VARCHAR, order_id INT, warehouse VARCHAR)
+CREATE STREAM shipments (ROWKEY VARCHAR KEY, shipment_id VARCHAR, ship_ts VARCHAR, order_id INT, warehouse VARCHAR)
     WITH (KAFKA_TOPIC='_shipments',
           VALUE_FORMAT='AVRO',
           TIMESTAMP='ship_ts',
@@ -14,11 +14,11 @@ CREATE STREAM shipments (shipment_id VARCHAR, ship_ts VARCHAR, order_id INT, war
 
 CREATE STREAM SHIPPED_ORDERS AS
     SELECT O.ROWKEY AS ORDER_ID,
-           TIMESTAMPTOSTRING(O.ROWTIME, 'yyyy-MM-dd HH:mm:ss') AS ORDER_TS,
+           TIMESTAMPTOSTRING(O.ROWTIME, 'yyyy-MM-dd HH:mm:ss', 'UTC') AS ORDER_TS,
            O.TOTAL_AMOUNT,
            O.CUSTOMER_NAME,
            S.SHIPMENT_ID,
-           TIMESTAMPTOSTRING(S.ROWTIME, 'yyyy-MM-dd HH:mm:ss') AS SHIPMENT_TS,
+           TIMESTAMPTOSTRING(S.ROWTIME, 'yyyy-MM-dd HH:mm:ss', 'UTC') AS SHIPMENT_TS,
            S.WAREHOUSE, (S.ROWTIME - O.ROWTIME) / 1000 / 60 AS SHIP_TIME
     FROM ORDERS O INNER JOIN SHIPMENTS S
     WITHIN 7 DAYS
--- _includes/tutorials/joining-table-table/ksql/code/tutorial-steps/dev/create-movies-table.sql
+++ _includes/tutorials/joining-table-table/ksql/code/tutorial-steps/dev/create-movies-table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE movies (id INT, title VARCHAR, release_year INT)
+CREATE TABLE movies (ROWKEY VARCHAR KEY, id INT, title VARCHAR, release_year INT)
              WITH (KAFKA_TOPIC='movies', 
                    PARTITIONS=1, 
                    VALUE_FORMAT='avro');
