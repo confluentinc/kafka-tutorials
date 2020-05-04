e9353b45d320eaff35f9fbb16e3c89e14cf043e6
Andy Coates <big-andy-coates@users.noreply.github.com>
2020-05-05 00:56:18 +0100

    updated with latest rc.
    - removed output files from git for ksql tutorials, as these are created on each run.
    - fixed a few tutorials that were not closing down their docker images
    - switch ksql to use `-d` when bringing up docker to stop the output being spammed.
    - updated ksql template with above.
    - updated PRINT TOPIC output check to include topic key.
    - Added an 'Aliens' movie to some tutorials, as `Die Hard` happens to be eight bytes, which means `PRINT TOPIC` will treat it as a `BIGINT` if its the first key it sees - leading to a misprint of the key in the example.  Adding `Aliens` fixes this.
    - Updated all print topic output to new rowtime format.

--- _includes/tutorials/joining-table-table/ksql/code/tutorial-steps/dev/populate-lead-actors.sql
+++ _includes/tutorials/joining-table-table/ksql/code/tutorial-steps/dev/populate-lead-actors.sql
@@ -1,3 +1,4 @@
+INSERT INTO LEAD_ACTOR (ROWKEY, TITLE, ACTOR_NAME) VALUES ('Aliens','Aliens','Sigourney Weaver');
 INSERT INTO LEAD_ACTOR (ROWKEY, TITLE, ACTOR_NAME) VALUES ('Die Hard','Die Hard','Bruce Willis');
 INSERT INTO LEAD_ACTOR (ROWKEY, TITLE, ACTOR_NAME) VALUES ('The Big Lebowski','The Big Lebowski','Jeff Bridges');
 INSERT INTO LEAD_ACTOR (ROWKEY, TITLE, ACTOR_NAME) VALUES ('The Godfather','The Godfather','Al Pacino');
