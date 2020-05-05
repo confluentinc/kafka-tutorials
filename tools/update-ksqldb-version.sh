208a9864512c7d5091d40860c765b7b2f6794dbb
Colin Hicks <colin.hicks@gmail.com>
2020-05-04 22:21:13 -0400

    should match on fqd image repos

--- tools/update-ksqldb-version.sh
+++ tools/update-ksqldb-version.sh
@@ -12,7 +12,7 @@ set -e -u -o pipefail
 # ./tools/update-ksqldb-version.sh 0.8.1
 
 version=$1
-registry=$2
+registry=${2:-}
 repo_prefix=
 
 if [[ -n "$registry" ]]; then
@@ -25,4 +25,4 @@ cd "$(dirname $0)/.."
 
 # both the xargs and sed incanations may need to be changed for non-macOS contexts
 find _includes/tutorials/**/ksql -name docker-compose.yml \
-    | xargs -I {} sed -i '' -E "s/confluentinc\/(ksqldb.+):[0-9]\.[0-9]\.[0-9]/$repo_prefix\1:$version/g" {}
+    | xargs -I {} sed -i '' -E "s/image:.*confluentinc\/(ksqldb.+):.+/image: $repo_prefix\1:$version/g" {}
