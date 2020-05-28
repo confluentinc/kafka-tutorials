5c398b657c33495304cec1fb653a24420f599ddb
Bill Bejeck <bbejeck@gmail.com>
2020-05-28 18:09:49 -0400

    remove use of '/usr/bin' from docker-compose commands

--- _includes/tutorials/console-consumer-produer-basic/kafka/code/tutorial-steps/dev/harness-console-consumer.sh
+++ _includes/tutorials/console-consumer-produer-basic/kafka/code/tutorial-steps/dev/harness-console-consumer.sh
@@ -1,1 +1,1 @@
-docker-compose exec broker /usr/bin/kafka-console-consumer --topic example-topic --bootstrap-server broker:9092 --max-messages 4
+docker-compose exec broker kafka-console-consumer --topic example-topic --bootstrap-server broker:9092 --max-messages 4
