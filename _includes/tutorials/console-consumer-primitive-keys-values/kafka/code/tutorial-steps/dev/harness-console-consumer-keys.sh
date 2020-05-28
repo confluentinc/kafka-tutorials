7d551901070cba6c6da370e0dc4e27258a69d734
Bill Bejeck <bbejeck@gmail.com>
2020-05-28 17:45:59 -0400

    update to remove '/usr/bin' from docker-compose exec commands

--- _includes/tutorials/console-consumer-primitive-keys-values/kafka/code/tutorial-steps/dev/harness-console-consumer-keys.sh
+++ _includes/tutorials/console-consumer-primitive-keys-values/kafka/code/tutorial-steps/dev/harness-console-consumer-keys.sh
@@ -1,4 +1,4 @@
-docker-compose exec broker /usr/bin/kafka-console-consumer --topic example --bootstrap-server broker:9092 \
+docker-compose exec broker kafka-console-consumer --topic example --bootstrap-server broker:9092 \
  --from-beginning \
  --property print.key=true \
  --property key.separator=" : " \
