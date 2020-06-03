d3d5878dcc7f08c889cf6d0f78b87c4620b1614f
Andy Coates <big-andy-coates@users.noreply.github.com>
2020-06-03 18:25:53 +0100

    update stream-table join tutorial inline with changes to ksqlDB master

--- _includes/tutorials/joining-stream-table/ksql/code/tutorial-steps/dev/continuous-join.sql
+++ _includes/tutorials/joining-stream-table/ksql/code/tutorial-steps/dev/continuous-join.sql
@@ -1,7 +1,6 @@
 CREATE STREAM rated_movies
     WITH (kafka_topic='rated_movies',
-          partitions=1,
           value_format='avro') AS
-    SELECT ratings.rowkey, title, rating
+    SELECT ratings.movie_id as id, title, rating
     FROM ratings
-    LEFT JOIN movies ON ratings.rowkey = movies.rowkey;
+    LEFT JOIN movies ON ratings.movie_id = movies.id;
