f4886dfb46fcaf89f9279207fc4e1faee7cfe42f
Andy Coates <big-andy-coates@users.noreply.github.com>
2020-06-03 18:11:17 +0100

    update stream-stream join tutorial inline with changes to ksqlDB master

--- _includes/tutorials/joining-stream-stream/ksql/code/tutorial-steps/dev/create-orders-stream.sql
+++ _includes/tutorials/joining-stream-stream/ksql/code/tutorial-steps/dev/create-orders-stream.sql
@@ -1,4 +1,4 @@
-CREATE STREAM orders (ROWKEY INT KEY, order_ts VARCHAR, total_amount DOUBLE, customer_name VARCHAR)
+CREATE STREAM orders (ID INT KEY, order_ts VARCHAR, total_amount DOUBLE, customer_name VARCHAR)
     WITH (KAFKA_TOPIC='_orders',
           VALUE_FORMAT='AVRO',
           TIMESTAMP='order_ts',
