d9f86c6f18b86533cc4b7535d471037fb5df1d2b
Andy Coates <big-andy-coates@users.noreply.github.com>
2020-06-04 16:19:11 +0100

    update serializaton tutorial inline with changes to ksqlDB master

--- _includes/tutorials/serialization/ksql/code/tutorial-steps/dev/create-json-movies.sql
+++ _includes/tutorials/serialization/ksql/code/tutorial-steps/dev/create-json-movies.sql
@@ -1,4 +1,4 @@
-CREATE STREAM movies_json (ROWKEY BIGINT KEY, title VARCHAR, release_year INT)
+CREATE STREAM movies_json (MOVIE_ID BIGINT KEY, title VARCHAR, release_year INT)
     WITH (KAFKA_TOPIC='json-movies',
           PARTITIONS=1,
           VALUE_FORMAT='json');
