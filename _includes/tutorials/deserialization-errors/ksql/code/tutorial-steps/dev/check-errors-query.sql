dba7a8dbdc25982737bc8b3e5daea9884a8a75ab
Andy Coates <big-andy-coates@users.noreply.github.com>
2020-06-05 14:34:03 +0100

    filter by topic

--- _includes/tutorials/deserialization-errors/ksql/code/tutorial-steps/dev/check-errors-query.sql
+++ _includes/tutorials/deserialization-errors/ksql/code/tutorial-steps/dev/check-errors-query.sql
@@ -1,7 +1,7 @@
 SELECT
-    MESSAGE->DESERIALIZATIONERROR->ERRORMESSAGE,
-    encode(MESSAGE->DESERIALIZATIONERROR->RECORDB64, 'base64', 'utf8') AS MSG,
-    MESSAGE->DESERIALIZATIONERROR->CAUSE
+    message->deserializationError->errorMessage,
+    encode(message->deserializationError->RECORDB64, 'base64', 'utf8') AS MSG,
+    message->deserializationError->cause
   FROM KSQL_PROCESSING_LOG
   EMIT CHANGES
   LIMIT 1;
