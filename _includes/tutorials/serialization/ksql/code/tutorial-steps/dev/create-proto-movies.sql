133850d64b986a79f63d94e97ce3390e825e7cc2
Viktor Gamov <viktor@confluent.io>
2020-06-26 18:06:28 -0400

    [KSQL] Avro to Protobuf tutorial update (#375)
    * Changing the tutorial to Avro2Proto
    * Fixing test
    
    * Re-enable kstreams tutorials
    
    * Adding missing sql
    
    * Enabling kstreams tutorial

--- _includes/tutorials/serialization/ksql/code/tutorial-steps/dev/create-proto-movies.sql
+++ _includes/tutorials/serialization/ksql/code/tutorial-steps/dev/create-proto-movies.sql
@@ -1,8 +1,3 @@
-CREATE STREAM movies_avro (ROWKEY BIGINT KEY, title VARCHAR, release_year INT)
-    WITH (KAFKA_TOPIC='avro-movies',
-          PARTITIONS=1,
-          VALUE_FORMAT='avro');
-
 CREATE STREAM movies_proto
     WITH (KAFKA_TOPIC='proto-movies', VALUE_FORMAT='protobuf') AS
     SELECT
