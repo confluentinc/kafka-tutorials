2022ba10ee1125641d0f1c6bb0c481dd053efd91
Yeva Byzek <yeva@confluent.io>
2020-08-03 07:42:47 -0400

    Initial checkin

---
+++ _includes/tutorials/kafka-connect-datagen-ccloud/kafka/code/tutorial-steps/dev/wait-for-containers.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Wait for Kafka Connect to become available
+while : 
+  do curl_status=$(curl -s -o /dev/null -w %{http_code} http://localhost:8083)
+  echo -e $(date) " Kafka Connect HTTP state: " $curl_status " (waiting for 200)" 
+  if [ $curl_status -eq 200 ] 
+    then  break
+  fi
+  sleep 5 
+done
+
+# Wait for Schema Registry to become available
+while : 
+  do curl_status=$(curl -s -o /dev/null -w %{http_code} http://localhost:8081)
+  echo -e $(date) " Schema Registry HTTP state: " $curl_status " (waiting for 200)" 
+  if [ $curl_status -eq 200 ] 
+    then  break
+  fi
+  sleep 5 
+done
