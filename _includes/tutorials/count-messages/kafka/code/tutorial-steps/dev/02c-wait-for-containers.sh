77723e3cbebd8124b85bea66257991eebb29c6fd
Robin Moffatt <robin@rmoff.net>
2020-09-24 14:26:34 +0100

    Add visual rendering of requirement to wait for components to start

--- _includes/tutorials/count-messages/kafka/code/tutorial-steps/dev/02c-wait-for-containers.sh
+++ _includes/tutorials/count-messages/kafka/code/tutorial-steps/dev/02c-wait-for-containers.sh
@@ -1,3 +1,1 @@
-# Since we're using ksqlDB with embedded connect, we wait for it to start
-# up so that the datagen connector is running before we continue. 
-docker exec -it ksqldb bash -c 'echo -e "\n\n  Waiting for ksqlDB to be available before launching CLI\n"; while : ; do curl_status=$(curl -s -o /dev/null -w %{http_code} http://ksqldb:8088/info) ; echo -e $(date) " ksqlDB server listener HTTP state: " $curl_status " (waiting for 200)" ; if [ $curl_status -eq 200 ] ; then  break ; fi ; sleep 5 ; done'
+docker exec -it ksqldb bash -c 'echo -e "\n\n  Waiting for startup… \n"; while : ; do curl_status=$(curl -s -o /dev/null -w %{http_code} http://ksqldb:8088/info) ; echo -e $(date) " ksqlDB server listener HTTP state: " $curl_status " (waiting for 200)" ; if [ $curl_status -eq 200 ] ; then  break ; fi ; sleep 5 ; done'
