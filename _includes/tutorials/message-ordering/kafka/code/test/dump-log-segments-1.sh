530f3cf8121dda260bae77df4075b5efd6429fc5
Yeva Byzek <yeva@confluent.io>
2020-10-09 21:37:32 -0400

    Tweak

--- _includes/tutorials/message-ordering/kafka/code/test/dump-log-segments-1.sh
+++ _includes/tutorials/message-ordering/kafka/code/test/dump-log-segments-1.sh
@@ -1,4 +1,4 @@
 docker-compose exec broker kafka-run-class kafka.tools.DumpLogSegments \
---print-data-log \ 
---files /var/lib/kafka/data/myTopic-1/00000000000000000000.log \
---deep-iteration
+  --print-data-log \ 
+  --files /var/lib/kafka/data/myTopic-1/00000000000000000000.log \
+  --deep-iteration
