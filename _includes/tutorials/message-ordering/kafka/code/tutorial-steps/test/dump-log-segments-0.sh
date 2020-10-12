445be4c82ab50c8abb6be792afcb4e9d9e035e0c
Yeva Byzek <yeva@confluent.io>
2020-10-11 20:01:02 -0400

    website and make succeess

--- _includes/tutorials/message-ordering/kafka/code/tutorial-steps/test/dump-log-segments-0.sh
+++ _includes/tutorials/message-ordering/kafka/code/tutorial-steps/test/dump-log-segments-0.sh
@@ -1,4 +1,4 @@
 docker-compose exec broker kafka-run-class kafka.tools.DumpLogSegments \
-  --print-data-log \ 
-  --files /var/lib/kafka/data/myTopic-0/00000000000000000000.log \
+  --print-data-log \
+  --files '/var/lib/kafka/data/myTopic-0/00000000000000000000.log' \
   --deep-iteration
