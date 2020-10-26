2f244aead130964f725b6cc34cbefedee56543c4
awalther28 <12937975+awalther28@users.noreply.github.com>
2020-10-26 17:37:12 -0600

    DEVX-2184; create new statement file for Prod

--- _includes/tutorials/anomaly-detection/ksql/code/tutorial-steps/test/run-tests.sh
+++ _includes/tutorials/anomaly-detection/ksql/code/tutorial-steps/test/run-tests.sh
@@ -1,1 +1,1 @@
-docker exec ksqldb-cli ksql-test-runner -s /opt/app/src/statements.sql -o /opt/app/test/output.json
+docker exec ksqldb-cli ksql-test-runner -s /opt/app/src/test-statements.sql -o /opt/app/test/output.json
