6a99a97dbb191a78a38fc8fb0d6ab4b9e4c9b30c
Yeva Byzek <yeva@confluent.io>
2020-11-18 16:56:46 -0500

    GH-600-dynamic: #600 add another short answer to the dynamic topic KT

---
+++ _includes/tutorials/dynamic-output-topic/kstreams/code/tutorial-steps/dev/answer-short.java
@@ -0,0 +1,11 @@
+final TopicNameExtractor <Long, CompletedOrder> orderTopicNameExtractor = (key, completedOrder, recordContext) -> {
+      final String compositeId = completedOrder.getId();
+      final String skuPart = compositeId.substring(compositeId.indexOf('-') + 1, 5);
+      final String outTopic;
+      if (skuPart.equals("QUA")) {
+           outTopic = specialOrderOutput;
+      } else {
+           outTopic = orderOutputTopic;
+      }
+      return outTopic;
+};
