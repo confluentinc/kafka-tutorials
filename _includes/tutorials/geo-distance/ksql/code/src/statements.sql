361d0dd7024713f4cdf78a0645355a443cd19ec7
awalther28 <12937975+awalther28@users.noreply.github.com>
2020-12-29 17:37:22 -0700

    DEVX-2351: add Testing It section

--- _includes/tutorials/geo-distance/ksql/code/src/statements.sql
+++ _includes/tutorials/geo-distance/ksql/code/src/statements.sql
@@ -1,15 +1,15 @@
-CREATE TABLE movies (TITLE VARCHAR PRIMARY KEY, id INT, release_year INT)
-             WITH (KAFKA_TOPIC='movies', 
-                   PARTITIONS=1, 
-                   VALUE_FORMAT='avro');
+CREATE TABLE repair_center_tab (repair_state VARCHAR PRIMARY KEY, long DOUBLE, lat DOUBLE)
+       WITH (kafka_topic='repair_center', value_format='avro', partitions=1);
 
-CREATE TABLE lead_actor (TITLE VARCHAR PRIMARY KEY, actor_name VARCHAR)
-             WITH (KAFKA_TOPIC='lead_actors', 
-                   PARTITIONS=1, 
-                   VALUE_FORMAT='avro');
+CREATE STREAM insurance_event_stream (customer_name VARCHAR, phone_model VARCHAR, event VARCHAR,
+                                      state VARCHAR, long DOUBLE, lat DOUBLE)
+       WITH (kafka_topic='phone_event_raw', value_format='avro', partitions=1);
 
-CREATE TABLE MOVIES_ENRICHED AS 
-  SELECT M.ID, M.TITLE, M.RELEASE_YEAR, L.ACTOR_NAME 
-  FROM MOVIES M 
-  INNER JOIN LEAD_ACTOR L 
-  ON M.TITLE = L.TITLE;
+CREATE STREAM insurance_event_with_repair_info AS
+SELECT * FROM insurance_event_stream iev
+INNER JOIN repair_center_tab rct ON iev.state = rct.repair_state;
+
+CREATE STREAM insurance_event_dist AS
+SELECT iev_customer_name, iev_state,
+              geo_distance(iev_lat, iev_long, rct_lat, rct_long, 'km') AS dist_to_repairer_km
+FROM insurance_event_with_repair_info;
