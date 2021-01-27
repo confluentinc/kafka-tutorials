4e07ddf495a15446a05245f8a07dbbdb1b921a6c
Chittaranjan Prasad <cprasad@confluent.io>
2021-01-26 21:21:44 -0800

    change 32

--- _includes/tutorials/rekeying/ksql/code/src/statements.sql
+++ _includes/tutorials/rekeying/ksql/code/src/statements.sql
@@ -1,6 +1,6 @@
 CREATE STREAM ratings (id INT, rating DOUBLE)
     WITH (kafka_topic='ratings', 
-          partitions=2, 
+          partitions=2,
           value_format='avro');
 
 CREATE STREAM RATINGS_REKEYED 
