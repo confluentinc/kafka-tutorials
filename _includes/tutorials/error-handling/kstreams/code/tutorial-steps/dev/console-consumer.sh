f72ab246f56c9af3fbac4927d0bb69aa3d808fb5
Bill Bejeck <bbejeck@gmail.com>
2021-02-25 18:59:36 -0500

    Adding more steps for tutorial

--- _includes/tutorials/error-handling/kstreams/code/tutorial-steps/dev/console-consumer.sh
+++ _includes/tutorials/error-handling/kstreams/code/tutorial-steps/dev/console-consumer.sh
@@ -1,8 +1,6 @@
 docker-compose exec broker kafka-console-consumer \
  --bootstrap-server broker:9092 \
  --topic output-topic \
- --property print.key=true \
- --property key.separator=" : "  \
  --from-beginning \
- --max-messages 4
+ --max-messages 15
 
