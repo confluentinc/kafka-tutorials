4c2eaccdbedfc75e21a049bd5daca509815ba883
awalther28 <12937975+awalther28@users.noreply.github.com>
2021-03-09 16:39:37 -0700

    DEVX-2488: clean up and add more files

--- _includes/tutorials/connect-sink-timestamp/ksql/code/tutorial-steps/dev/create-sink-connector.sql
+++ _includes/tutorials/connect-sink-timestamp/ksql/code/tutorial-steps/dev/create-sink-connector.sql
@@ -1,12 +1,12 @@
 CREATE SINK CONNECTOR JDBC_SINK_POSTGRES_01 WITH (
-    'connector.class'= 'io.confluent.connect.jdbc.JdbcSourceConnector',
-    'connection.url'= 'jdbc:postgresql://postgres:5432/postgres',
-    'connection.user'= 'postgres',
-    'connection.password'= 'postgres',
-    'topics'= 'deviceEvents',
-    'key.converter'       = 'org.apache.kafka.connect.storage.StringConverter',
-    'auto.create'         = 'true',
-    'auto.evolve'         = 'true',
-    'transforms.setTimestampType.field'= 'eventTime',
+    'connector.class' = 'io.confluent.connect.jdbc.JdbcSourceConnector',
+    'connection.url' = 'jdbc:postgresql://postgres:5432/postgres',
+    'connection.user' = 'postgres',
+    'connection.password' = 'postgres',
+    'topics' = 'deviceEvents',
+    'auto.create' = 'true',
+    'auto.evolve' = 'true',
+    'topic.prefix' = '',
+    'mode' = 'bulk',
+    'transforms.setTimestampType.field' = 'eventTime'
 );
-
