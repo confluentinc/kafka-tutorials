dcae2f463ab5ad631d28e5d05230e603a0589027
Yeva Byzek <yeva@confluent.io>
2021-03-10 12:15:40 -0500

    Fix sliding windows datagen

--- _includes/tutorials/kafka-streams-schedule-operations/kstreams/code/tutorial-steps/dev/wait-for-datagen-topic.sh
+++ _includes/tutorials/kafka-streams-schedule-operations/kstreams/code/tutorial-steps/dev/wait-for-datagen-topic.sh
@@ -1,35 +1,11 @@
 #!/bin/bash
 
 # Wait for the datagen topic to be created
-#while : 
-#  do output=$(docker-compose exec broker kafka-topics --bootstrap-server localhost:29092 --topic login-events --describe)
-#  if [[ "$output" =~ "Topic: login-events" ]]; then
-#    echo "Topic login-events exists"
-#    break
-#  else
-#    echo "Connect logs: "
-#    docker-compose logs connect
-#  fi
-#  sleep 5 
-#done
-
-
-echo " test docker container /tmp/:"
-docker-compose exec connect ls /tmp/
-
-echo " test docker container /tmp/datagen-logintime.avsc:"
-docker-compose exec connect ls /tmp/datagen-logintime.avsc
-
-# Wait for the datagen topic to be created
-sleep 20
-
-echo "connect logs:"
-docker-compose logs connect
-
-output=$(docker-compose exec broker kafka-topics --bootstrap-server localhost:29092 --topic login-events --describe)
-echo "$output"
-if [[ "$output" =~ "Topic: login-events" ]]; then
-  echo "Topic login-events exists"
-else
-  echo "Topic does not exist"
-fi
+while : 
+  do output=$(docker-compose exec broker kafka-topics --bootstrap-server localhost:29092 --topic login-events --describe)
+  if [[ "$output" =~ "Topic: login-events" ]]; then
+    echo "Topic login-events exists"
+    break
+  fi
+  sleep 5 
+done
