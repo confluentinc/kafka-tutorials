7beb266b93804228b9495eeb9a2f11014c08e4ff
Yeva Byzek <yeva@confluent.io>
2021-03-10 08:47:31 -0500

    More troubleshooting

--- _includes/tutorials/kafka-streams-schedule-operations/kstreams/code/tutorial-steps/dev/wait-for-datagen-topic.sh
+++ _includes/tutorials/kafka-streams-schedule-operations/kstreams/code/tutorial-steps/dev/wait-for-datagen-topic.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
 # Wait for the datagen topic to be created
-while : 
-  do output=$(docker-compose exec broker kafka-topics --bootstrap-server localhost:29092 --topic login-events --describe)
-  if [[ "$output" =~ "Topic: login-events" ]]; then
-    echo "Topic login-events exists"
-    break
-  else
-    echo "Connect logs: "
-    docker-compose logs connect
-  fi
-  sleep 5 
-done
+#while : 
+#  do output=$(docker-compose exec broker kafka-topics --bootstrap-server localhost:29092 --topic login-events --describe)
+#  if [[ "$output" =~ "Topic: login-events" ]]; then
+#    echo "Topic login-events exists"
+#    break
+#  else
+#    echo "Connect logs: "
+#    docker-compose logs connect
+#  fi
+#  sleep 5 
+#done
+
+
+# Wait for the datagen topic to be created
+sleep 20
+
+echo "avsc:"
+docker-compose exec connect ls /schemas/
+
+echo "connect logs:"
+docker-compose logs connect
+
+output=$(docker-compose exec broker kafka-topics --bootstrap-server localhost:29092 --topic login-events --describe)
+echo "$output"
+if [[ "$output" =~ "Topic: login-events" ]]; then
+  echo "Topic login-events exists"
+else
+  echo "Topic does not exist"
+fi
