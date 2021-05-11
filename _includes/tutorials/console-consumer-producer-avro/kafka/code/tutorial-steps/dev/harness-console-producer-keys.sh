6bf2d80864f0e8e1c5cd7d141908ed1c8c9b5042
Yeva Byzek <yeva@confluent.io>
2021-05-11 03:32:52 -0400

    Consistency on space

--- _includes/tutorials/console-consumer-producer-avro/kafka/code/tutorial-steps/dev/harness-console-producer-keys.sh
+++ _includes/tutorials/console-consumer-producer-avro/kafka/code/tutorial-steps/dev/harness-console-producer-keys.sh
@@ -1,5 +1,5 @@
 docker-compose exec -T schema-registry kafka-avro-console-producer --topic example-topic-avro --bootstrap-server broker:9092\
   --property key.schema='{"type":"string"}' \
   --property value.schema="$(< schema/order_detail.avsc)"\
-  --property parse.key=true\
+  --property parse.key=true \
   --property key.separator=":"
--- _includes/tutorials/console-consumer-read-specific-offsets-partition/kafka/code/tutorial-steps/dev/harness-console-producer-keys.sh
+++ _includes/tutorials/console-consumer-read-specific-offsets-partition/kafka/code/tutorial-steps/dev/harness-console-producer-keys.sh
@@ -1,3 +1,3 @@
 docker-compose exec -T broker kafka-console-producer --topic example-topic --bootstrap-server broker:9092\
-  --property parse.key=true\
+  --property parse.key=true \
   --property key.separator=":"
