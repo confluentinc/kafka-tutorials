e4927b5051f5a6ad01cfa3c79f53b71d660805ed
Sarwar Bhuiyan <sarwar@confluent.io>
2021-07-12 10:41:56 +0100

    Tutorial for expiring data in KTable from aggregate

---
+++ _includes/tutorials/schedule-ktable-ttl-ii/kstreams/code/tutorial-steps/dev/console-consumer-2.sh
@@ -0,0 +1,8 @@
+docker exec -it broker kafka-console-consumer \
+ --bootstrap-server broker:9092 \
+ --topic output-topic \
+ --property print.key=true \
+ --value-deserializer "org.apache.kafka.common.serialization.StringDeserializer" \
+ --property key.separator=" : "  \
+ --from-beginning \
+ --max-messages 6
