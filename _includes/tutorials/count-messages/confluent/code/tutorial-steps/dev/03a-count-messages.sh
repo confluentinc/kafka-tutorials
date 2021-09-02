89066545632013cae9bf00e49d6cf9c244de6c9d
Yeva Byzek <ybyzek@users.noreply.github.com>
2021-09-02 17:11:01 -0400

    CCloudify count messages tutorial (#930)

---
+++ _includes/tutorials/count-messages/confluent/code/tutorial-steps/dev/03a-count-messages.sh
@@ -0,0 +1,7 @@
+docker run -it --network=host \
+    -v ${PWD}/configuration/ccloud.properties:/tmp/configuration/ccloud.properties \
+    edenhill/kcat:1.7.0 kafkacat \
+         -F /tmp/configuration/ccloud.properties \
+         -C -t test-topic \
+         -e -q \
+         | grep -v "Reading configuration from file" | wc -l
