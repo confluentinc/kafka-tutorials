b6933a37fe71b624ffc10296497b02d5cfc00c07
Yeva Byzek <yeva@confluent.io>
2021-10-02 19:59:01 -0400

    Passing test

--- _includes/tutorials/console-consumer-producer-avro/kafka/code/tutorial-steps/dev/console-consumer-keys.sh
+++ _includes/tutorials/console-consumer-producer-avro/kafka/code/tutorial-steps/dev/console-consumer-keys.sh
@@ -2,6 +2,7 @@ kafka-avro-console-consumer \
   --topic orders-avro \
   --bootstrap-server broker:9092 \
   --property schema.registry.url=http://localhost:8081 \
-  --from-beginning \
+  --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
   --property print.key=true \
-  --property key.separator="-"
+  --property key.separator="-" \
+  --from-beginning
--- _includes/tutorials/console-consumer-producer-avro/kafka/code/tutorial-steps/dev/harness-console-consumer-keys.sh
+++ _includes/tutorials/console-consumer-producer-avro/kafka/code/tutorial-steps/dev/harness-console-consumer-keys.sh
@@ -2,6 +2,7 @@ kafka-avro-console-consumer \
   --topic orders-avro \
   --property schema.registry.url=http://localhost:8081 \
   --bootstrap-server broker:9092 \
+  --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
   --property print.key=true \
   --property key.separator="-" \
   --from-beginning
