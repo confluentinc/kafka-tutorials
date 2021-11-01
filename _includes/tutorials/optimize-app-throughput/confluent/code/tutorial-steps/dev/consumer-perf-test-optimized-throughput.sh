046dbcf76f49a85c51c56a2acc0fb63fee7da81b
Yeva Byzek <yeva@confluent.io>
2021-11-01 19:44:56 -0400

    Add consumer

---
+++ _includes/tutorials/optimize-app-throughput/confluent/code/tutorial-steps/dev/consumer-perf-test-optimized-throughput.sh
@@ -0,0 +1,7 @@
+docker run -v $PWD/configuration/ccloud.properties:/etc/ccloud.properties confluentinc/cp-server:6.2.1 /usr/bin/kafka-consumer-perf-test \
+    --topic topic-perf \
+    --messages 5000 \
+    --bootstrap-server $(grep bootstrap.servers $PWD/configuration/ccloud.properties) \
+    --consumer.config /etc/ccloud.properties \
+    --consumer-props \
+        fetch.min.bytes=100000
--- _includes/tutorials/optimize-app-throughput/confluent/code/tutorial-steps/dev/producer-perf-test-default.sh
+++ _includes/tutorials/optimize-app-throughput/confluent/code/tutorial-steps/dev/producer-perf-test-default.sh
@@ -1,5 +1,5 @@
 docker run -v $PWD/configuration/ccloud.properties:/etc/ccloud.properties confluentinc/cp-server:6.2.1 /usr/bin/kafka-producer-perf-test \
-    --topic output-topic \
+    --topic topic-perf \
     --num-records 5000 \
     --record-size 5000 \
     --throughput -1 \
