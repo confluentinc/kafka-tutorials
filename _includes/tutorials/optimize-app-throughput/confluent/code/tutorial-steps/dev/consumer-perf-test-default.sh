ae52da503a4f3724933f67223eb0752771bd17ab
Yeva Byzek <yeva@confluent.io>
2021-11-02 09:49:56 -0400

    Focus on producer only

--- _includes/tutorials/optimize-app-throughput/confluent/code/tutorial-steps/dev/consumer-perf-test-default.sh
+++
@@ -1,5 +0,0 @@
-docker run -v $PWD/configuration/ccloud.properties:/etc/ccloud.properties confluentinc/cp-server:6.2.1 /usr/bin/kafka-consumer-perf-test \
-    --topic topic-perf \
-    --messages 10000 \
-    --bootstrap-server $(grep bootstrap.servers $PWD/configuration/ccloud.properties) \
-    --consumer.config /etc/ccloud.properties
