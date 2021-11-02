d2862a63831ceede3b9a15fb5fadd100e6dd3908
Yeva Byzek <yeva@confluent.io>
2021-11-01 20:04:00 -0400

    Modify record sizes

--- _includes/tutorials/optimize-app-throughput/confluent/code/tutorial-steps/dev/consumer-perf-test-default.sh
+++ _includes/tutorials/optimize-app-throughput/confluent/code/tutorial-steps/dev/consumer-perf-test-default.sh
@@ -1,5 +1,5 @@
 docker run -v $PWD/configuration/ccloud.properties:/etc/ccloud.properties confluentinc/cp-server:6.2.1 /usr/bin/kafka-consumer-perf-test \
     --topic topic-perf \
-    --messages 5000 \
+    --messages 10000 \
     --bootstrap-server $(grep bootstrap.servers $PWD/configuration/ccloud.properties) \
     --consumer.config /etc/ccloud.properties
--- _includes/tutorials/optimize-app-throughput/confluent/code/tutorial-steps/dev/producer-perf-test-default.sh
+++ _includes/tutorials/optimize-app-throughput/confluent/code/tutorial-steps/dev/producer-perf-test-default.sh
@@ -1,6 +1,6 @@
 docker run -v $PWD/configuration/ccloud.properties:/etc/ccloud.properties confluentinc/cp-server:6.2.1 /usr/bin/kafka-producer-perf-test \
     --topic topic-perf \
-    --num-records 5000 \
-    --record-size 5000 \
+    --num-records 10000 \
+    --record-size 8000 \
     --throughput -1 \
     --producer.config /etc/ccloud.properties
