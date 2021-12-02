2b04391905a81b5aa9a8ce3d801d7f3ae0dcf613
Daniel Wade <wade.daniel.w@gmail.com>
2021-12-01 18:02:39 -0800

    updated to extend nav features to tutorial pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,15 +51,4 @@ $(function() {
         $('.more-101').toggleClass('is-hidden');
         return false;
     });
-
-    // Toggles mobile menu open/closed
-    $('.mobile-menu').on('click', function(e){
-      $('.mobile-menu, .nav-mobile').toggleClass('open');
-    });
-
-    // Toggles accordion in mobile menu open/closed
-    $('.accordion-item-wrapper').on('click', function() {
-      $(this).toggleClass('collapsed');
-      $(this).find('.arrow').toggleClass('down').toggleClass('up');
-    });
 });
