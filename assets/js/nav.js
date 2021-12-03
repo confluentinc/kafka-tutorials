103b0a27ec6324326ffdd6244b7d71f2e134c0f3
Daniel Wade <wade.daniel.w@gmail.com>
2021-12-02 17:31:34 -0800

    updated fonts and scroll styles

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,5 +1,23 @@
+const SCROLL_CLASSES = [
+  'header',
+  'logo-container',
+  'mobile-menu'
+].join(',.');
+
+$(window).on('scroll', function(){
+
+  // If scrolling & no scrolled classes
+  if (window.scrollY > 0 && !$(SCROLL_CLASSES).hasClass('scrolled')) {
+    $(SCROLL_CLASSES).addClass('scrolled');
+
+  // If at top of page & scrolled classes
+  } else if(window.scrollY === 0 && $(SCROLL_CLASSES).hasClass('scrolled')) {
+    $(SCROLL_CLASSES).removeClass('scrolled');
+  }
+});
+
 // Toggles mobile menu open/closed
-$('.mobile-menu').on('click', function(e){
+$('.mobile-menu').on('click', function(){
   $('.mobile-menu, .nav-mobile').toggleClass('open');
 });
 
