eb1e4acc49885a7f47ac180eaed63018cdb9446a
Bill Bejeck <bbejeck@gmail.com>
2021-12-06 17:29:40 -0500

    updates per comments - remove join word

--- _includes/tutorials/multiple-event-types/kafka/code/tutorial-steps/prod/launch-container.sh
+++ _includes/tutorials/multiple-event-types/kafka/code/tutorial-steps/prod/launch-container.sh
@@ -1,1 +1,1 @@
-docker run -v $PWD/configuration/prod.properties:/config.properties io.confluent.developer/multiple-event-types-join:0.0.1 config.properties
+docker run -v $PWD/configuration/prod.properties:/config.properties io.confluent.developer/multiple-event-types:0.0.1 config.properties
