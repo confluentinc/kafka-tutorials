2c2f10eb3d7680e70cfb5c28d82fad8e19a56163
Yeva Byzek <yeva@confluent.io>
2022-01-26 08:18:34 -0500

    Add logistics recipe

---
+++ _includes/tutorials/logistics/confluent/code/tutorial-steps/dev/source.sql
@@ -0,0 +1,33 @@
+CREATE SOURCE CONNECTOR orders WITH (
+  'connector.class'          = 'PostgresSource',
+  'name'                     = 'recipe-postgres-logistics-orders',
+  'kafka.api.key'            = '<my-kafka-api-key>',
+  'kafka.api.secret'         = '<my-kafka-api-secret>',
+  'connection.host'          = '<my-database-endpoint>',
+  'connection.port'          = '5432',
+  'connection.user'          = 'postgres',
+  'connection.password'      = '<my-database-password>',
+  'db.name'                  = '<db-name>',
+  'table.whitelist'          = 'orders',
+  'timestamp.column.name'    = 'timestamp',
+  'output.data.format'       = 'JSON',
+  'db.timezone'              = 'UTC',
+  'tasks.max'                = '1'
+);
+
+CREATE SOURCE CONNECTOR fleet_updates WITH (
+  'connector.class'          = 'PostgresSource',
+  'name'                     = 'recipe-postgres-logistics-fleet',
+  'kafka.api.key'            = '<my-kafka-api-key>',
+  'kafka.api.secret'         = '<my-kafka-api-secret>',
+  'connection.host'          = '<my-database-endpoint>',
+  'connection.port'          = '5432',
+  'connection.user'          = 'postgres',
+  'connection.password'      = '<my-database-password>',
+  'db.name'                  = '<db-name>',
+  'table.whitelist'          = 'fleet_updates',
+  'timestamp.column.name'    = 'timestamp',
+  'output.data.format'       = 'JSON',
+  'db.timezone'              = 'UTC',
+  'tasks.max'                = '1'
+);
