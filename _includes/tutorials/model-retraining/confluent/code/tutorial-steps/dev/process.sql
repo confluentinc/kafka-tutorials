7839cd57130e3416ff8f38ed9f5d65b840312ea7
Yeva Byzek <yeva@confluent.io>
2022-02-04 08:35:40 -0500

    Clean up process.sql; case; formatting; quotes

--- _includes/tutorials/model-retraining/confluent/code/tutorial-steps/dev/process.sql
+++ _includes/tutorials/model-retraining/confluent/code/tutorial-steps/dev/process.sql
@@ -2,13 +2,12 @@ SET 'auto.offset.reset' = 'earliest';
 
 -- Create stream of predictions
 CREATE STREAM predicted_weight(
-  "Fish_Id" VARCHAR KEY,
-  "Species" VARCHAR,
-  "Height" DOUBLE,
-  "Length" DOUBLE,
-  "Prediction" DOUBLE
-  )
-WITH(
+  fish_id VARCHAR KEY,
+  species VARCHAR,
+  height DOUBLE,
+  length DOUBLE,
+  prediction DOUBLE
+) WITH (
   KAFKA_TOPIC = 'kt.mdb.weight-prediction', 
   VALUE_FORMAT = 'JSON',
   PARTITIONS = 6
@@ -16,51 +15,42 @@ WITH(
 
 -- Create stream of actual weights
 CREATE STREAM actual_weight(
-  "Fish_Id" VARCHAR KEY,
-  "Species" VARCHAR,
-  "Weight" DOUBLE
-  )
-WITH(
+  fish_id VARCHAR KEY,
+  species VARCHAR,
+  weight DOUBLE
+) WITH (
   KAFKA_TOPIC = 'kt.mdb.machine-weight', 
   VALUE_FORMAT = 'JSON',
   PARTITIONS = 6
 );
 
 -- Create stream joining predictions with actual weights
-CREATE STREAM diff_weight
-WITH(
-  KAFKA_TOPIC = 'diff_weight', 
-  VALUE_FORMAT = 'JSON'
-)
-AS SELECT
-   -- This fake Key field will give us something to group by in the next step
-   'Key' AS "Key", 
-   predicted_weight."Fish_Id" AS "Fish_Id",
-   predicted_weight."Species" AS "Species",
-   predicted_weight."Length" AS "Length",
-   predicted_weight."Height" AS "Height",
-   predicted_weight."Prediction" AS "PredictedWeight",
-   actual_weight."Weight" AS "ActualWeight",
-   ROUND(ABS(predicted_weight."Prediction" - actual_weight."Weight") / actual_weight."Weight", 3) AS "Error"
+CREATE STREAM diff_weight WITH (KAFKA_TOPIC = 'diff_weight') AS
+  SELECT
+   -- This fake key field will give us something to group by in the next step
+   'key' AS key, 
+   predicted_weight.fish_id AS fish_id,
+   predicted_weight.species AS species,
+   predicted_weight.length AS length,
+   predicted_weight.height AS height,
+   predicted_weight.prediction AS prediction,
+   actual_weight.weight AS actual,
+   ROUND(ABS(predicted_weight.prediction - actual_weight.weight) / actual_weight.weight, 3) AS Error
 FROM predicted_weight
 INNER JOIN actual_weight
 WITHIN 1 MINUTE
 GRACE PERIOD 1 MINUTE
-ON predicted_weight."Fish_Id" = actual_weight."Fish_Id";
+ON predicted_weight.fish_id = actual_weight.fish_id;
 
 -- Create table of one minute aggregates with over 15% error rate
-CREATE TABLE retrain_weight
-WITH(
-  KAFKA_TOPIC = 'retrain_weight', 
-  VALUE_FORMAT = 'JSON'
-)
-AS SELECT
-   "Key",
-   COLLECT_SET("Species") AS "Species",
-   EARLIEST_BY_OFFSET("Fish_Id") AS "Fish_Id_Start",
-   LATEST_BY_OFFSET("Fish_Id") AS "Fish_Id_End",
-   AVG("Error") AS "ErrorAvg"
-FROM DIFF_WEIGHT
+CREATE TABLE retrain_weight WITH (KAFKA_TOPIC = 'retrain_weight') AS
+ SELECT
+   key,
+   COLLECT_SET(species) AS species,
+   EARLIEST_BY_OFFSET(fish_id) AS fish_id_start,
+   LATEST_BY_OFFSET(fish_id) AS fish_id_end,
+   AVG(Error) AS ErrorAvg
+FROM diff_weight
 WINDOW TUMBLING (SIZE 1 MINUTE, GRACE PERIOD 1 MINUTE)
-GROUP BY "Key"
-HAVING ROUND(AVG(DIFF_WEIGHT.`Error`), 2) > 0.15;
+GROUP BY key
+HAVING ROUND(AVG(diff_weight.Error), 2) > 0.15;
