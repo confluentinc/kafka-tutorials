89a0883fdaece855fd2b98c19b4560890e969db8
Yeva Byzek <yeva@confluent.io>
2022-02-07 07:29:10 -0500

    Reformat aviation

--- _includes/tutorials/aviation/confluent/code/tutorial-steps/dev/c02.sql
+++ _includes/tutorials/aviation/confluent/code/tutorial-steps/dev/c02.sql
@@ -1,16 +1,22 @@
-CREATE TABLE flights (ID               INT     PRIMARY KEY
-                       , ORIGIN        VARCHAR
-                       , DESTINATION   VARCHAR
-                       , CODE          VARCHAR
-                       , SCHEDULED_DEP TIMESTAMP
-                       , SCHEDULED_ARR TIMESTAMP)
-              WITH (KAFKA_TOPIC='flights'
-                   , FORMAT='JSON'
-                   , PARTITIONS=6);
+CREATE TABLE flights (
+  ID INT PRIMARY KEY,
+  ORIGIN VARCHAR,
+  DESTINATION VARCHAR,
+  CODE VARCHAR,
+  SCHEDULED_DEP TIMESTAMP,
+  SCHEDULED_ARR TIMESTAMP
+) WITH (
+  KAFKA_TOPIC='flights',
+  FORMAT='JSON',
+  PARTITIONS=6
+);
 
-CREATE TABLE bookings (ID            INT     PRIMARY KEY
-                       , CUSTOMER_ID INT
-                       , FLIGHT_ID   INT)
-              WITH (KAFKA_TOPIC='bookings'
-                   , FORMAT='JSON'
-                   , PARTITIONS=6);
+CREATE TABLE bookings (
+  ID INT PRIMARY KEY,
+  CUSTOMER_ID INT,
+  FLIGHT_ID INT
+) WITH (
+  KAFKA_TOPIC='bookings',
+  FORMAT='JSON',
+  PARTITIONS=6
+);
