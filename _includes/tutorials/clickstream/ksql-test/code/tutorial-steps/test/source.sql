c005c4b3df7c426fe6cdd8f3dd35fe0e4b595628
Yeva Byzek <ybyzek@users.noreply.github.com>
2022-03-09 11:09:21 -0500

    Create test case for recipe: clickstream (#1126)

--- _includes/tutorials/clickstream/ksql-test/code/tutorial-steps/test/source.sql
+++ _includes/tutorials/clickstream/ksql-test/code/tutorial-steps/test/source.sql
@@ -1,9 +1,7 @@
 -- Stream of users
 CREATE SOURCE CONNECTOR DATAGEN_USERS WITH (
   'name'                     = 'DATAGEN_USERS',
-  'connector.class'          = 'DatagenSource',
-  'kafka.api.key'            = '<my-kafka-api-key>',
-  'kafka.api.secret'         = '<my-kafka-api-secret>',
+  'connector.class'          = 'io.confluent.kafka.connect.datagen.DatagenConnector',
   'kafka.topic'              = 'clickstream_users',
   'quickstart'               = 'CLICKSTREAM_USERS',
   'maxInterval'              = '10',
@@ -14,9 +12,7 @@ CREATE SOURCE CONNECTOR DATAGEN_USERS WITH (
 -- Stream of per-user session information
 CREATE SOURCE CONNECTOR DATAGEN_CLICKS WITH (
   'name'                     = 'DATAGEN_CLICKS',
-  'connector.class'          = 'DatagenSource',
-  'kafka.api.key'            = '<my-kafka-api-key>',
-  'kafka.api.secret'         = '<my-kafka-api-secret>',
+  'connector.class'          = 'io.confluent.kafka.connect.datagen.DatagenConnector',
   'kafka.topic'              = 'clickstream',
   'quickstart'               = 'CLICKSTREAM',
   'maxInterval'              = '30',
