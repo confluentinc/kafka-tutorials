4aa6463368d328d5b54fc8dd8a3e0e1ac4df0450
Yeva Byzek <yeva@confluent.io>
2022-03-17 10:51:11 -0400

    Augment aviation recipe with MASK and sinks

--- _includes/tutorials/aviation/confluent/code/tutorial-steps/dev/manual.sql
+++ _includes/tutorials/aviation/confluent/code/tutorial-steps/dev/manual.sql
@@ -3,11 +3,11 @@
 --  the flight data to be processed by the customer_flights_rekeyed
 --  table before the JOIN with the flight updates data
 
-INSERT INTO customers (id, name, address, email, phone, loyalty_status) VALUES (1, 'Gleda Lealle', '93 Express Point', 'glealle0@senate.gov', '+351 831 301 6746', 'Silver');
-INSERT INTO customers (id, name, address, email, phone, loyalty_status) VALUES (2, 'Gilly Crocombe', '332 Blaine Avenue', 'gcrocombe1@homestead.com', '+33 203 565 3736', 'Silver');
-INSERT INTO customers (id, name, address, email, phone, loyalty_status) VALUES (3, 'Astrix Aspall', '56 Randy Place', 'aaspall2@ebay.co.uk', '+33 679 296 6645', 'Gold');
-INSERT INTO customers (id, name, address, email, phone, loyalty_status) VALUES (4, 'Ker Omond', '23255 Tennessee Court', 'komond3@usnews.com', '+33 515 323 0170', 'Silver');
-INSERT INTO customers (id, name, address, email, phone, loyalty_status) VALUES (5, 'Arline Synnott', '144 Ramsey Avenue', 'asynnott4@theatlantic.com', '+62 953 759 8885', 'Bronze');
+INSERT INTO customers (id, name, address, email, phone, loyalty_status, loyalty_id) VALUES (1, 'Gleda Lealle', '93 Express Point', 'glealle0@senate.gov', '+351 831 301 6746', 'Silver', '2PLNX338063');
+INSERT INTO customers (id, name, address, email, phone, loyalty_status, loyalty_id) VALUES (2, 'Gilly Crocombe', '332 Blaine Avenue', 'gcrocombe1@homestead.com', '+33 203 565 3736', 'Silver', '7AWLM918339');
+INSERT INTO customers (id, name, address, email, phone, loyalty_status, loyalty_id) VALUES (3, 'Astrix Aspall', '56 Randy Place', 'aaspall2@ebay.co.uk', '+33 679 296 6645', 'Gold', '3RNZH870911');
+INSERT INTO customers (id, name, address, email, phone, loyalty_status, loyalty_id) VALUES (4, 'Ker Omond', '23255 Tennessee Court', 'komond3@usnews.com', '+33 515 323 0170', 'Silver', '5BWEP418137');
+INSERT INTO customers (id, name, address, email, phone, loyalty_status, loyalty_id) VALUES (5, 'Arline Synnott', '144 Ramsey Avenue', 'asynnott4@theatlantic.com', '+62 953 759 8885', 'Bronze', '4MNJB877136');
 
 INSERT INTO flights (id, origin, destination, code, scheduled_dep, scheduled_arr) VALUES (1, 'LBA', 'AMS', '642',  '2021-11-18T06:04:00', '2021-11-18T06:48:00');
 INSERT INTO flights (id, origin, destination, code, scheduled_dep, scheduled_arr) VALUES (2, 'LBA', 'LHR', '9607', '2021-11-18T07:36:00', '2021-11-18T08:05:00');
--- _includes/tutorials/aviation/confluent/code/tutorial-steps/dev/r01.sql
+++ _includes/tutorials/aviation/confluent/code/tutorial-steps/dev/r01.sql
@@ -5,6 +5,7 @@ CREATE STREAM cf_stream (
   cb_c_email VARCHAR,
   cb_c_phone VARCHAR,
   cb_c_loyalty_status VARCHAR,
+  cb_c_loyalty_id VARCHAR,
   cb_flight_id INTEGER,
   f_id INTEGER,
   f_origin VARCHAR,
@@ -18,7 +19,7 @@ CREATE STREAM cf_stream (
   VALUE_FORMAT = 'JSON'
 );
 
-CREATE STREAM cf_rekey WITH (KAFKA_TOPIC = 'cf_rekey') AS
+CREATE STREAM cf_rekey_masked WITH (KAFKA_TOPIC = 'cf_rekey_masked') AS
   SELECT f_id           AS flight_id,
     cb_c_id             AS customer_id,
     cb_c_name           AS customer_name,
@@ -26,6 +27,7 @@ CREATE STREAM cf_rekey WITH (KAFKA_TOPIC = 'cf_rekey') AS
     cb_c_email          AS customer_email,
     cb_c_phone          AS customer_phone,
     cb_c_loyalty_status AS customer_loyalty_status,
+    MASK_KEEP_RIGHT(cb_c_loyalty_id,3) AS customer_loyalty_id,
     f_origin            AS flight_origin,
     f_destination       AS flight_destination,
     f_code              AS flight_code,
@@ -42,13 +44,14 @@ CREATE TABLE customer_flights_rekeyed (
   customer_email VARCHAR,
   customer_phone VARCHAR,
   customer_loyalty_status VARCHAR,
+  customer_loyalty_id VARCHAR,
   flight_origin VARCHAR,
   flight_destination VARCHAR,
   flight_code VARCHAR,
   flight_scheduled_dep TIMESTAMP,
   flight_scheduled_arr TIMESTAMP
 ) WITH (
-  KAFKA_TOPIC = 'cf_rekey',
+  KAFKA_TOPIC = 'cf_rekey_masked',
   KEY_FORMAT = 'KAFKA',
   VALUE_FORMAT = 'JSON'
 );
