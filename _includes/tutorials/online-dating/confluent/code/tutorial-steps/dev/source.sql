40524814865720085eb1cf990013423654fb76cf
Yeva Byzek <ybyzek@users.noreply.github.com>
2022-03-23 14:10:36 -0400

    [MINOR] Change colon to equal sign; text in parameters for user to fill in (#1201)
    * Change colon to equal sign
    
    * Consistency in text to fill in

--- _includes/tutorials/online-dating/confluent/code/tutorial-steps/dev/source.sql
+++ _includes/tutorials/online-dating/confluent/code/tutorial-steps/dev/source.sql
@@ -4,14 +4,14 @@ CREATE SOURCE CONNECTOR online_dating WITH (
   'name'                     = 'recipe-postgres-dating',
   'kafka.api.key'            = '<my-kafka-api-key>',
   'kafka.api.secret'         = '<my-kafka-api-secret>',
-  'connection.host'          = '<database-endpoint>',
-  'connection.port'          = '<database-endpoint>',
+  'connection.host'          = '<database-host>',
+  'connection.port'          = '<database-port>',
   'connection.user'          = '<database-user>',
   'connection.password'      = '<database-password>',
   'database'                 = '<database-name>',
-  'table.whitelist'          : 'messages',
-  'timestamp.column.name'    : 'created_at',
-  'output.data.format'       : 'JSON',
-  'db.timezone'              : 'UTC',
-  'tasks.max'                : '1'
+  'table.whitelist'          = 'messages',
+  'timestamp.column.name'    = 'created_at',
+  'output.data.format'       = 'JSON',
+  'db.timezone'              = 'UTC',
+  'tasks.max'                = '1'
 );
