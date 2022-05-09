269e55b0d9b6bc52ee562a7b0d768eb1bf15b36c
Bill Bejeck <bbejeck@gmail.com>
2022-05-09 15:21:57 -0400

    Updates gradle fix console consumer script

--- _includes/tutorials/error-handling/kstreams/code/tutorial-steps/dev/console-consumer.sh
+++ _includes/tutorials/error-handling/kstreams/code/tutorial-steps/dev/console-consumer.sh
@@ -1,6 +1,6 @@
-docker-compose exec broker kafka-console-consumer \
+docker-compose exec -T broker kafka-console-consumer \
  --bootstrap-server broker:9092 \
  --topic output-topic \
  --from-beginning \
- --max-messages 12
+ --max-messages 6
 
