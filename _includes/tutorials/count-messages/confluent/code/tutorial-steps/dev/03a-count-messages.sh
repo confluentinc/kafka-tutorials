97abd26500f6267956da2c5406a40fb3680740da
Yeva Byzek <yeva@confluent.io>
2022-05-31 13:47:24 -0400

    Rename kafkacat to kcat

--- _includes/tutorials/count-messages/confluent/code/tutorial-steps/dev/03a-count-messages.sh
+++ _includes/tutorials/count-messages/confluent/code/tutorial-steps/dev/03a-count-messages.sh
@@ -1,6 +1,6 @@
 docker run -it --network=host \
     -v ${PWD}/configuration/ccloud.properties:/tmp/configuration/ccloud.properties \
-    edenhill/kcat:1.7.0 kafkacat \
+    edenhill/kcat:1.7.0 kcat \
          -F /tmp/configuration/ccloud.properties \
          -C -t test-topic \
          -e -q \
--- _includes/tutorials/count-messages/kafka/code/tutorial-steps/dev/04b-count-messages-ccloud.sh
+++ _includes/tutorials/count-messages/kafka/code/tutorial-steps/dev/04b-count-messages-ccloud.sh
@@ -1,4 +1,4 @@
-docker run --rm edenhill/kafkacat:1.6.0 \
+docker run --rm edenhill/kcat:1.7.1 \
     -X security.protocol=SASL_SSL -X sasl.mechanisms=PLAIN \
     -X ssl.ca.location=./etc/ssl/cert.pem -X api.version.request=true \
     -X sasl.username="${CCLOUD_API_KEY}" \
