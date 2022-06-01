52a5ab19d671169d27f1457eef2c8f79d28b7d02
Yeva Byzek <ybyzek@users.noreply.github.com>
2022-06-01 11:08:48 -0400

    Rename kafkacat to kcat (#1277)

--- _includes/tutorials/generate-test-data-streams/kafka/code/tutorial-steps/dev/consume-topic-02b.sh
+++ _includes/tutorials/generate-test-data-streams/kafka/code/tutorial-steps/dev/consume-topic-02b.sh
@@ -1,4 +1,4 @@
-docker exec -i kafkacat kafkacat -b broker:9092 -t traffic \
+docker exec -i kcat kcat -b broker:9092 -t traffic \
             -s key=s -s value=avro -r http://schema-registry:8081        \
             -f 'Key     (%K bytes):\t%k\nPayload (%S bytes):\t%s\n--\n' \
             -C -c5 -o end -u 2>/dev/null            
