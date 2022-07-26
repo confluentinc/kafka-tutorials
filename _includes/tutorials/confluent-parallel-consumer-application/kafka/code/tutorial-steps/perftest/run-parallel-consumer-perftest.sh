d709429213a6793719ac78be3cee8c893c4deb95
Dave Troiano <dtroiano@confluent.io>
2022-07-26 17:24:46 -0400

    Confluent Parallel Consumer tutorial

---
+++ _includes/tutorials/confluent-parallel-consumer-application/kafka/code/tutorial-steps/perftest/run-parallel-consumer-perftest.sh
@@ -0,0 +1,1 @@
+java -cp build/libs/confluent-parallel-consumer-application-standalone-0.0.1-all.jar io.confluent.developer.ParallelConsumerPerfTest configuration/perftest-parallel-consumer.properties
