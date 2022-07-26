2ed33b77e6865cb83ca2398a9e96236fbcd3e247
Dave Troiano <dtroiano@confluent.io>
2022-07-26 17:24:46 -0400

    Confluent Parallel Consumer tutorial

---
+++ _includes/tutorials/confluent-parallel-consumer-application/kafka/code/src/main/java/io/confluent/developer/ParallelConsumerApplication.java
@@ -0,0 +1,70 @@
+package io.confluent.developer;
+
+import io.confluent.parallelconsumer.ParallelConsumerOptions;
+import io.confluent.parallelconsumer.ParallelStreamProcessor;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Properties;
+
+import static io.confluent.parallelconsumer.ParallelConsumerOptions.CommitMode.PERIODIC_CONSUMER_SYNC;
+import static io.confluent.parallelconsumer.ParallelConsumerOptions.ProcessingOrder.KEY;
+
+public class ParallelConsumerApplication {
+  private ConsumerRecordHandler<String, String> recordHandler;
+  private ParallelStreamProcessor<String, String> parallelConsumer;
+
+  public ParallelConsumerApplication(final ParallelStreamProcessor<String, String> parallelConsumer,
+                                          final ConsumerRecordHandler<String, String> recordsHandler) {
+    this.parallelConsumer = parallelConsumer;
+    this.recordHandler = recordsHandler;
+  }
+
+  public void runConsume(final Properties consumerProps) {
+      parallelConsumer.subscribe(Collections.singletonList(consumerProps.getProperty("input.topic.name")));
+      parallelConsumer.poll(context -> recordHandler.process(context.getSingleConsumerRecord()));
+  }
+
+  public void shutdown() {
+    parallelConsumer.close();
+  }
+
+  public static Properties loadProperties(String fileName) throws IOException {
+    final Properties props = new Properties();
+    final FileInputStream input = new FileInputStream(fileName);
+    props.load(input);
+    input.close();
+    return props;
+  }
+
+  public static void main(String[] args) throws Exception {
+
+    if (args.length < 1) {
+      throw new IllegalArgumentException(
+          "This program takes one argument: the path to an environment configuration file.");
+    }
+
+    final Properties consumerAppProps = ParallelConsumerApplication.loadProperties(args[0]);
+    final String filePath = consumerAppProps.getProperty("file.path");
+    final Consumer<String, String> consumer = new KafkaConsumer<>(consumerAppProps);
+    final ParallelConsumerOptions options = ParallelConsumerOptions.<String, String>builder()
+            .ordering(KEY)
+            .maxConcurrency(1000)
+            .consumer(consumer)
+            .commitMode(PERIODIC_CONSUMER_SYNC)
+            .build();
+    ParallelStreamProcessor<String, String> eosStreamProcessor =
+            ParallelStreamProcessor.createEosStreamProcessor(options);
+    final ConsumerRecordHandler<String, String> recordsHandler = new FileWritingRecordHandler(Paths.get(filePath));
+    final ParallelConsumerApplication consumerApplication = new ParallelConsumerApplication(eosStreamProcessor, recordsHandler);
+
+    Runtime.getRuntime().addShutdownHook(new Thread(consumerApplication::shutdown));
+
+    consumerApplication.runConsume(consumerAppProps);
+  }
+
+}
---
+++ _includes/tutorials/confluent-parallel-consumer-application/kafka/code/tutorial-steps/dev/print-consumer-file-results.sh
@@ -0,0 +1,1 @@
+cat parallel-consumer-records.out
