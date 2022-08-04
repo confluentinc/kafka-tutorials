a1b3961fc6e8a17f5c28f15bd885cb4edf48a5cd
Hao Li <1127478+lihaosky@users.noreply.github.com>
2022-08-04 16:19:39 -0700

    set python version

--- harness_runner/setup.py
+++ harness_runner/setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(
     name='harness-runner',
     version='0.0.1',
-    py_modules=['util', 'harness_runner', 'ksql'],
     scripts=['harness-runner'],
-    install_requires=['pyyaml==6.0']
+    py_modules=[]
 )
