452d9ea4201b9e7df0a96c4b7cdd14595a15119e
Dave Troiano <dtroiano@confluent.io>
2022-09-21 09:36:00 -0400

    punchlist of recipe fixes
    * remove `name` property from `CREATE CONNECTOR` statements
    * connector name consistency
    * whitespace consistency
    * topic name casing consistency

--- _includes/tutorials/generate-test-data-streams/ksql/code/src/statements.sql
+++ _includes/tutorials/generate-test-data-streams/ksql/code/src/statements.sql
@@ -1,7 +1,6 @@
 CREATE SOURCE CONNECTOR IF NOT EXISTS CLICKS WITH (
     'connector.class'             = 'io.mdrogalis.voluble.VolubleSourceConnector',
     'key.converter'               = 'org.apache.kafka.connect.storage.StringConverter',
-    
     'genkp.clicks.with'           = '#{Number.randomDigit}',
     'attrkp.clicks.null.rate'     = 1,
     'genv.clicks.source_ip.with'  = '#{Internet.ipV4Address}',
@@ -12,18 +11,16 @@ CREATE SOURCE CONNECTOR IF NOT EXISTS CLICKS WITH (
 );
 
 CREATE SOURCE CONNECTOR IF NOT EXISTS NETWORK_TRAFFIC WITH (
-    'connector.class'                = 'io.mdrogalis.voluble.VolubleSourceConnector',
-    'key.converter'                  = 'org.apache.kafka.connect.storage.StringConverter',
-
-    'genkp.devices.with'                   = '#{Internet.macAddress}',
-     'genv.devices.name.with'              = '#{GameOfThrones.dragon}',
-     'genv.devices.location->city.with'    = '#{Address.city}',
-     'genv.devices.location->country.with' = '#{Address.country}',
-    'topic.devices.records.exactly'        = 10,
-
-     'genkp.traffic.with'                = '#{Number.randomDigit}',
-    'attrkp.traffic.null.rate'           = 1,
-      'genv.traffic.mac.matching'        = 'devices.key',
-      'genv.traffic.bytes_sent.with'     = '#{Number.numberBetween ''64'',''4096''}',
-     'topic.traffic.throttle.ms'         = 500 
+    'connector.class'                     = 'io.mdrogalis.voluble.VolubleSourceConnector',
+    'key.converter'                       = 'org.apache.kafka.connect.storage.StringConverter',
+    'genkp.devices.with'                  = '#{Internet.macAddress}',
+    'genv.devices.name.with'              = '#{GameOfThrones.dragon}',
+    'genv.devices.location->city.with'    = '#{Address.city}',
+    'genv.devices.location->country.with' = '#{Address.country}',
+    'topic.devices.records.exactly'       = 10,
+    'genkp.traffic.with'                  = '#{Number.randomDigit}',
+    'attrkp.traffic.null.rate'            = 1,
+    'genv.traffic.mac.matching'           = 'devices.key',
+    'genv.traffic.bytes_sent.with'        = '#{Number.numberBetween ''64'',''4096''}',
+    'topic.traffic.throttle.ms'           = 500
 );
