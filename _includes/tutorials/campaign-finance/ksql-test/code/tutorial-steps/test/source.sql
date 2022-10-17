fc99124d3b485e93571e7819d2ab875b9d15eb15
Dave Troiano <dtroiano@confluent.io>
2022-10-17 12:04:02 -0400

    update campaign finance recipe to use datagen

--- _includes/tutorials/campaign-finance/ksql-test/code/tutorial-steps/test/source.sql
+++ _includes/tutorials/campaign-finance/ksql-test/code/tutorial-steps/test/source.sql
@@ -1,8 +1,6 @@
 -- Stream of campaign contributions
 CREATE SOURCE CONNECTOR IF NOT EXISTS recipe_datagen_campaign_finance WITH (
-  'connector.class'          = 'DatagenSource',
-  'kafka.api.key'            = '<my-kafka-api-key>',
-  'kafka.api.secret'         = '<my-kafka-api-secret>',
+  'connector.class'          = 'io.confluent.kafka.connect.datagen.DatagenConnector',
   'kafka.topic'              = 'campaign_finance',
   'quickstart'               = 'CAMPAIGN_FINANCE',
   'maxInterval'              = '10',
--- _includes/tutorials/campaign-finance/ksql-test/code/wait-for-containers.sh
+++ _includes/tutorials/campaign-finance/ksql-test/code/wait-for-containers.sh
@@ -13,3 +13,17 @@ while :
   fi
   sleep 5
 done
+
+# Wait for Kafka Connect to become available
+while :
+  do curl_status=$(curl -s -o /dev/null -w %{http_code} http://localhost:8083/connectors)
+  echo -e $(date) " Kafka Connect HTTP state: " $curl_status " (waiting for 200)"
+  if [ $curl_status -eq 200 ]
+    then
+      # Sleep an extra 10 seconds for Herder to start
+      sleep 10
+      echo "✅ Connect is ready"
+      break
+  fi
+  sleep 5
+done
