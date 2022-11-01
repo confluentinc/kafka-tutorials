a6475941ab985a04b7c090fc1de5924f6f806509
Dave Troiano <dtroiano@confluent.io>
2022-11-01 09:57:55 -0400

    update `docker-compose` commands to `docker compose`

--- _includes/tutorials/message-ordering/kafka/code/tutorial-steps/test/dump-log-segments-0.sh
+++ _includes/tutorials/message-ordering/kafka/code/tutorial-steps/test/dump-log-segments-0.sh
@@ -1,4 +1,4 @@
-docker-compose exec broker kafka-dump-log \
+docker exec -t broker kafka-dump-log \
   --print-data-log \
   --files '/var/lib/kafka/data/myTopic-0/00000000000000000000.log' \
   --deep-iteration
