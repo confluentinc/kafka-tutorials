a693fa2bb5b3d00d7be794485c0f9d0654c51049
Dave Troiano <dtroiano@confluent.io>
2022-11-10 14:41:17 -0500

    switch from deprecated `KStream.transformValues` to `processValues`

--- _includes/tutorials/finding-distinct/kstreams/code/src/main/java/io/confluent/developer/FindDistinctEvents.java
+++ _includes/tutorials/finding-distinct/kstreams/code/src/main/java/io/confluent/developer/FindDistinctEvents.java
@@ -10,29 +10,31 @@ import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KeyValueMapper;
 import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.kstream.ValueTransformerWithKey;
-import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.processor.api.ContextualFixedKeyProcessor;
+import org.apache.kafka.streams.processor.api.FixedKeyProcessor;
+import org.apache.kafka.streams.processor.api.FixedKeyProcessorContext;
+import org.apache.kafka.streams.processor.api.FixedKeyProcessorSupplier;
+import org.apache.kafka.streams.processor.api.FixedKeyRecord;
 import org.apache.kafka.streams.state.StoreBuilder;
 import org.apache.kafka.streams.state.Stores;
 import org.apache.kafka.streams.state.WindowStore;
 import org.apache.kafka.streams.state.WindowStoreIterator;
 
 import java.io.FileInputStream;
-import java.io.InputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.time.Duration;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 
 import io.confluent.common.utils.TestUtils;
 import io.confluent.developer.avro.Click;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
-
-import static io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG;
 
 public class FindDistinctEvents {
 
@@ -48,9 +50,7 @@ public class FindDistinctEvents {
      * <p>
      * Note: This code is for demonstration purposes and was not tested for production usage.
      */
-    private static class DeduplicationTransformer<K, V, E> implements ValueTransformerWithKey<K, V, V> {
-
-        private ProcessorContext context;
+    private static class DeduplicationProcessorSupplier<K, V, E> implements FixedKeyProcessorSupplier<K, V, V> {
 
         /**
          * Key: ip address
@@ -59,79 +59,97 @@ public class FindDistinctEvents {
          */
         private WindowStore<E, Long> eventIdStore;
 
+        private final Duration windowSize;
         private final long leftDurationMs;
         private final long rightDurationMs;
 
         private final KeyValueMapper<K, V, E> idExtractor;
 
         /**
-         * @param maintainDurationPerEventInMs how long to "remember" a known ip address
-         *                                     during the time of which any incoming duplicates
-         *                                     will be dropped, thereby de-duplicating the
-         *                                     input.
          * @param idExtractor                  extracts a unique identifier from a record by which we de-duplicate input
          *                                     records; if it returns null, the record will not be considered for
          *                                     de-duping but forwarded as-is.
          */
-        DeduplicationTransformer(final long maintainDurationPerEventInMs, final KeyValueMapper<K, V, E> idExtractor) {
-            if (maintainDurationPerEventInMs < 1) {
-                throw new IllegalArgumentException("maintain duration per event must be >= 1");
-            }
+        DeduplicationProcessorSupplier(final KeyValueMapper<K, V, E> idExtractor) {
+            // How long we "remember" an event.  During this time, any incoming duplicates of the event
+            // will be, well, dropped, thereby de-duplicating the input data.
+            //
+            // The actual value depends on your use case.  To reduce memory and disk usage, you could
+            // decrease the size to purge old windows more frequently at the cost of potentially missing out
+            // on de-duplicating late-arriving records.
+            windowSize = Duration.ofMinutes(2);
+
+            final long maintainDurationPerEventInMs = windowSize.toMillis();
             leftDurationMs = maintainDurationPerEventInMs / 2;
             rightDurationMs = maintainDurationPerEventInMs - leftDurationMs;
             this.idExtractor = idExtractor;
         }
 
         @Override
-        @SuppressWarnings("unchecked")
-        public void init(final ProcessorContext context) {
-            this.context = context;
-            eventIdStore = (WindowStore<E, Long>) context.getStateStore(storeName);
+        public Set<StoreBuilder<?>> stores() {
+
+            // retention period must be at least window size -- for this use case, we don't need a longer retention period
+            // and thus just use the window size as retention time
+            final Duration retentionPeriod = windowSize;
+
+            final StoreBuilder<WindowStore<String, Long>> dedupStoreBuilder = Stores.windowStoreBuilder(
+                Stores.persistentWindowStore(storeName,
+                    retentionPeriod,
+                    windowSize,
+                    false
+                ),
+                Serdes.String(),
+                Serdes.Long());
+
+            return Collections.singleton(dedupStoreBuilder);
         }
 
         @Override
-        public V transform(final K key, final V value) {
-            final E eventId = idExtractor.apply(key, value);
-            if (eventId == null) {
-                return value;
-            } else {
-                final V output;
-                if (isDuplicate(eventId)) {
-                    output = null;
-                    updateTimestampOfExistingEventToPreventExpiry(eventId, context.timestamp());
-                } else {
-                    output = value;
-                    rememberNewEvent(eventId, context.timestamp());
-                }
-                return output;
-            }
+        public FixedKeyProcessor<K, V, V> get() {
+            return new DeduplicationProcessor();
         }
 
-        private boolean isDuplicate(final E eventId) {
-            final long eventTime = context.timestamp();
-            final WindowStoreIterator<Long> timeIterator = eventIdStore.fetch(
+        private class DeduplicationProcessor extends ContextualFixedKeyProcessor<K, V, V> {
+
+            @Override
+            public void init(final FixedKeyProcessorContext<K, V> context) {
+                super.init(context);
+                eventIdStore = context().getStateStore(storeName);
+            }
+
+            @Override
+            public void process(final FixedKeyRecord<K, V> record) {
+                final E eventId = idExtractor.apply(record.key(), record.value());
+                if (eventId == null) {
+                    context().forward(record);
+                } else {
+                    if (isDuplicate(eventId, record.timestamp())) {
+                        updateTimestampOfExistingEventToPreventExpiry(eventId, record.timestamp());
+                    } else {
+                        rememberNewEvent(eventId, record.timestamp());
+                        context().forward(record);
+                    }
+                }
+            }
+
+            private boolean isDuplicate(final E eventId, long eventTime) {
+                final WindowStoreIterator<Long> timeIterator = eventIdStore.fetch(
                     eventId,
                     eventTime - leftDurationMs,
                     eventTime + rightDurationMs);
-            final boolean isDuplicate = timeIterator.hasNext();
-            timeIterator.close();
-            return isDuplicate;
-        }
+                final boolean isDuplicate = timeIterator.hasNext();
+                timeIterator.close();
+                return isDuplicate;
+            }
 
-        private void updateTimestampOfExistingEventToPreventExpiry(final E eventId, final long newTimestamp) {
-            eventIdStore.put(eventId, newTimestamp, newTimestamp);
-        }
+            private void updateTimestampOfExistingEventToPreventExpiry(final E eventId, final long newTimestamp) {
+                eventIdStore.put(eventId, newTimestamp, newTimestamp);
+            }
 
-        private void rememberNewEvent(final E eventId, final long timestamp) {
-            eventIdStore.put(eventId, timestamp, timestamp);
+            private void rememberNewEvent(final E eventId, final long timestamp) {
+                eventIdStore.put(eventId, timestamp, timestamp);
+            }
         }
-
-        @Override
-        public void close() {
-            // Note: The store should NOT be closed manually here via `eventIdStore.close()`!
-            // The Kafka Streams API will automatically close stores when necessary.
-        }
-
     }
 
     private SpecificAvroSerde<Click> buildClicksSerde(final Properties allProps) {
@@ -148,34 +166,11 @@ public class FindDistinctEvents {
         final String inputTopic = allProps.getProperty("input.topic.name");
         final String outputTopic = allProps.getProperty("output.topic.name");
 
-        // How long we "remember" an event.  During this time, any incoming duplicates of the event
-        // will be, well, dropped, thereby de-duplicating the input data.
-        //
-        // The actual value depends on your use case.  To reduce memory and disk usage, you could
-        // decrease the size to purge old windows more frequently at the cost of potentially missing out
-        // on de-duplicating late-arriving records.
-        final Duration windowSize = Duration.ofMinutes(2);
-
-        // retention period must be at least window size -- for this use case, we don't need a longer retention period
-        // and thus just use the window size as retention time
-        final Duration retentionPeriod = windowSize;
-
-        final StoreBuilder<WindowStore<String, Long>> dedupStoreBuilder = Stores.windowStoreBuilder(
-                Stores.persistentWindowStore(storeName,
-                        retentionPeriod,
-                        windowSize,
-                        false
-                ),
-                Serdes.String(),
-                Serdes.Long());
-
-        builder.addStateStore(dedupStoreBuilder);
-
         builder
-                .stream(inputTopic, Consumed.with(Serdes.String(), clicksSerde))
-                .transformValues(() -> new DeduplicationTransformer<>(windowSize.toMillis(), (key, value) -> value.getIp()), storeName)
-                .filter((k, v) -> v != null)
-                .to(outputTopic, Produced.with(Serdes.String(), clicksSerde));
+            .stream(inputTopic, Consumed.with(Serdes.String(), clicksSerde))
+            .processValues(new DeduplicationProcessorSupplier<>((key, value) -> value.getIp()), storeName)
+            .filter((k, v) -> v != null)
+            .to(outputTopic, Produced.with(Serdes.String(), clicksSerde));
 
         return builder.build();
     }
@@ -185,13 +180,13 @@ public class FindDistinctEvents {
 
         List<NewTopic> topics = new ArrayList<>();
         topics.add(new NewTopic(
-                allProps.getProperty("input.topic.name"),
-                Integer.parseInt(allProps.getProperty("input.topic.partitions")),
-                Short.parseShort(allProps.getProperty("input.topic.replication.factor"))));
+            allProps.getProperty("input.topic.name"),
+            Integer.parseInt(allProps.getProperty("input.topic.partitions")),
+            Short.parseShort(allProps.getProperty("input.topic.replication.factor"))));
         topics.add(new NewTopic(
-                allProps.getProperty("output.topic.name"),
-                Integer.parseInt(allProps.getProperty("output.topic.partitions")),
-                Short.parseShort(allProps.getProperty("output.topic.replication.factor"))));
+            allProps.getProperty("output.topic.name"),
+            Integer.parseInt(allProps.getProperty("output.topic.partitions")),
+            Short.parseShort(allProps.getProperty("output.topic.replication.factor"))));
 
         client.createTopics(topics);
         client.close();
@@ -209,7 +204,7 @@ public class FindDistinctEvents {
     public static void main(String[] args) throws IOException {
         if (args.length < 1) {
             throw new IllegalArgumentException(
-                    "This program takes one argument: the path to an environment configuration file.");
+                "This program takes one argument: the path to an environment configuration file.");
         }
 
         new FindDistinctEvents().runRecipe(args[0]);
