b9180bf851a77983052ae62453c80c605dfe6ca9
Terry <tleem@confluent.io>
2022-11-18 13:01:27 -0800

    addressed comments

--- assets/js/scrollDepth.js
+++ assets/js/scrollDepth.js
@@ -2,9 +2,7 @@ const SCROLL_DEPTH_LEVELS = [20, 40, 60, 80, 99];
 
 window.addEventListener('DOMContentLoaded', function () {
   let observer
-  const el = document.getElementsByTagName('body')[0];
-    
-  el.style.position = 'relative';
+  const el = document.body;
 
   const initObserver = () => {
     const cb = (entries) => {
@@ -13,6 +11,7 @@ window.addEventListener('DOMContentLoaded', function () {
     const position = Number(target.dataset.depthMarkerId);
 
       if (isIntersecting) {
+
         analytics.track('Scroll Depth', {
           scrollDepth: position === 99 ? 100 : position // triggering at 99% is more consistent than 100%.
         });
