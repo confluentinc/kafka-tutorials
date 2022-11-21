0845403ea3a8559d97a009ce59d4574632813916
Terry <75696892+TerryDevS@users.noreply.github.com>
2022-11-21 09:37:24 -0800

    [Segment] Scroll depth tracking (#1455)
    * Implemented scrollDepth event tracker
    
    * Added scrollDepth script
    
    * Added scrollDepth script
    
    * Cleanup
    
    * Updated position
    
    * addressed comments

---
+++ assets/js/scrollDepth.js
@@ -0,0 +1,45 @@
+const SCROLL_DEPTH_LEVELS = [20, 40, 60, 80, 99];
+
+window.addEventListener('DOMContentLoaded', function () {
+  let observer
+  const el = document.body;
+
+  const initObserver = () => {
+    const cb = (entries) => {
+    const [IntersectionObserverEntry] = entries;
+    const { isIntersecting, target } = IntersectionObserverEntry;
+    const position = Number(target.dataset.depthMarkerId);
+
+      if (isIntersecting) {
+
+        analytics.track('Scroll Depth', {
+          scrollDepth: position === 99 ? 100 : position // triggering at 99% is more consistent than 100%.
+        });
+    
+        observer.unobserve(target);
+      }
+    };
+
+     observer = new IntersectionObserver(cb);
+
+      SCROLL_DEPTH_LEVELS.forEach((position) => {
+        let markerEl = document.querySelector(
+          `[data-depth-marker-id="${position}"]`
+        );
+  
+        if (!markerEl) {
+          markerEl = document.createElement('i');
+          markerEl.style.position = 'absolute';
+          markerEl.style.top = `${position}%`;
+          markerEl.dataset.depthMarkerId = position;
+  
+          el.appendChild(markerEl);
+        }
+  
+        observer.observe(markerEl);
+      });
+    }
+
+    initObserver();
+  });
+  
