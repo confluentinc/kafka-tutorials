c2b9f39ff6a83660de921a2eabead3842c3e3113
Bill Bejeck <bbejeck@gmail.com>
2023-01-17 13:58:54 -0500

    touch for build

--- _includes/tutorials/cogrouping-streams/kstreams/code/src/main/java/io/confluent/developer/CogroupingStreams.java
+++ _includes/tutorials/cogrouping-streams/kstreams/code/src/main/java/io/confluent/developer/CogroupingStreams.java
@@ -43,7 +43,7 @@ public class CogroupingStreams {
 
 
     public Topology buildTopology(Properties allProps) {
-        //Touching the file to trigger a build
+        // Touching the file to trigger a build - should only trigger one section
         final StreamsBuilder builder = new StreamsBuilder();
         final String appOneInputTopic = allProps.getProperty("app-one.topic.name");
         final String appTwoInputTopic = allProps.getProperty("app-two.topic.name");
