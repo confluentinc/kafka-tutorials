f17b28f5d242a91c52ac56ba93c4581f82c0966b
Bill Bejeck <bbejeck@gmail.com>
2023-01-18 14:56:06 -0500

    Revert java classes

--- _includes/tutorials/cogrouping-streams/kstreams/code/src/main/java/io/confluent/developer/CogroupingStreams.java
+++ _includes/tutorials/cogrouping-streams/kstreams/code/src/main/java/io/confluent/developer/CogroupingStreams.java
@@ -43,7 +43,6 @@ public class CogroupingStreams {
 
 
     public Topology buildTopology(Properties allProps) {
-        // Another change 7!!
         final StreamsBuilder builder = new StreamsBuilder();
         final String appOneInputTopic = allProps.getProperty("app-one.topic.name");
         final String appTwoInputTopic = allProps.getProperty("app-two.topic.name");
