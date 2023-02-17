67cefd095e46525cdb8ddea51566a16f58b26637
Bill Bejeck <bbejeck@gmail.com>
2023-02-17 16:32:48 -0500

    final updates for clone script

--- tools/clone-tutorial.py
+++ tools/clone-tutorial.py
@@ -34,7 +34,7 @@ parser.add_argument('--new-name', required=True,
 parser.add_argument('--orig-main-class', help='Name of original main class (applicable for Java/Flink)')
 parser.add_argument('--new-main-class', help='Name of new main class (applicable for Java/Flink)')
 parser.add_argument('--semaphore-test-name', required=True, help='The name of the test for semaphore.yml file')
-parser.add_argument('--permalink', help='The permalink for the tutorial')
+parser.add_argument('--permalink', help='The permalink for the tutorial - more descriptive link')
 parser.add_argument('--json-file', help="The path to a json file containing these configurations")
 parser.add_argument('--debug', default=False, help='The permalink for the tutorial')
 
@@ -111,7 +111,7 @@ def update_semaphore_yaml(yaml, block_name, job):
 
 
 def update_tutorials_yaml(yaml, tutorial_name, enabled_tutorial_types):
-    if yaml[tutorial_name] is None:
+    if tutorial_name not in yaml:
         yaml[tutorial_name] = {
             'title': 'ADD A TITLE',
             'meta-description': 'ADD A META-DESCRIPTION',
@@ -135,34 +135,60 @@ def create_semaphore_job(semaphore_test_name, tutorial):
     }
 
 
+def _get_front_matter(tutorial_name, tutorial_type):
+    with open("../tutorials/{name}/{tutorial_type}.html".format(name=tutorial_name, tutorial_type=tutorial_type)) as sf:
+        front_matter = {}
+        for line in sf.readlines()[1:-1]:
+            vals = line.split(':')
+            front_matter[vals[0].strip()] = vals[1].strip()
+    return front_matter
+
+
+def update_front_matter(orig_name, new_name, old_type, new_type, new_permalink=None):
+    fm = _get_front_matter(orig_name, old_type)
+    fm['stack'] = new_type
+    fm['static_data'] = new_name
+    permalink = fm['permalink']
+    if new_permalink is None or (new_permalink is not None and (orig_name == new_name)):
+        fm['permalink'] = permalink.replace(old_type, new_type)
+    elif new_permalink is not None and (orig_name != new_name):
+        fm['permalink'] = '/' + new_permalink + '/' + new_type
+
+    new_path = "../tutorials/{name}".format(name=new_name)
+
+    if not os.path.exists(new_path):
+        os.mkdir(new_path)
+
+    with open(new_path + "/{tutorial_type}.html".format(tutorial_type=new_type), 'w') as sf:
+        sf.write('---\n')
+        for key, value in fm.items():
+            sf.write(key + ': ' + value + '\n')
+        sf.write('---\n')
+
+
 def handle_harness_files(orig_single_type,
                          new_single_type,
-                         test_harness_dir,
-                         orig_tutorial_name,
-                         new_tutorial_name):
-    if orig_single_type != '' and new_single_type != '':
-        orig_harness_file = test_harness_dir + '/' + orig_tutorial_name + '/' + orig_single_type + '.yml'
-        new_harness_file = test_harness_dir + '/' + new_tutorial_name + '/' + new_single_type + '.yml'
-        shutil.copy(orig_harness_file, new_harness_file)
-        replace_names_in_file(new_harness_file, args.orig_name, args.new_name)
-    else:
-        orig_test_harness_dir = test_harness_dir + '/' + orig_tutorial_name
-        new_test_harness_dir = test_harness_dir + '/' + new_tutorial_name
-        shutil.copytree(orig_test_harness_dir, new_test_harness_dir)
-        yaml_files_for_substitution = get_file_paths_for_replacement(new_test_harness_dir)
-        for yf in yaml_files_for_substitution:
-            replace_names_in_file(yf, orig_tutorial_name, new_tutorial_name)
+                         test_hrns_dir,
+                         orig_tut_name,
+                         new_tut_name):
+    orig_harness_file = test_hrns_dir + '/' + orig_tut_name + '/' + orig_single_type + '.yml'
+    new_harness_file = test_hrns_dir + '/' + new_tut_name + '/' + new_single_type + '.yml'
+    if not os.path.exists(test_hrns_dir + '/' + new_tut_name):
+        os.mkdir(test_hrns_dir + '/' + new_tut_name)
+    shutil.copy(orig_harness_file, new_harness_file)
+    replace_names_in_file(new_harness_file, args.orig_name, args.new_name)
+    return [new_single_type]
 
 
 def ensure_valid_tutorial_names():
     if os.path.exists(proposed_tutorial):
         print("A tutorial for %s exists" % proposed_tutorial)
         exit(1)
-    if orig_tutorial_type != '' and orig_tutorial_type not in tutorial_types:
-        print("Tutorial type %s unknown" % orig_tutorial_type)
+    if orig_tutorial_type == '' or orig_tutorial_type not in tutorial_types:
+        print("Tutorial type %s is either unknown or must be provided" % orig_tutorial_type)
         exit(1)
-    elif new_tutorial_type != '' and new_tutorial_type not in tutorial_types:
-        print("Tutorial type %s unknown" % new_tutorial_type)
+    elif new_tutorial_type == '' or new_tutorial_type not in tutorial_types:
+        print("Tutorial type %s is either unknown or must be provided" % new_tutorial_type)
         exit(1)
 
 
@@ -199,8 +225,8 @@ shutil.copytree(original_tutorial, proposed_tutorial)
 
 print("Copy the needed YAML files to %s" % test_harness_dir + '/' + new_tutorial_name)
 print("Then do the required replacements for getting the correct paths")
-handle_harness_files(orig_tutorial_type, new_tutorial_type,
-                     test_harness_dir, orig_tutorial_name, new_tutorial_name)
+types_to_enable = handle_harness_files(orig_tutorial_type, new_tutorial_type,
+                                       test_harness_dir, orig_tutorial_name, new_tutorial_name)
 
 print("Updating copied files from {old} to {new}".format(old=args.orig_name, new=args.new_name))
 do_replacements(proposed_tutorial, args.orig_name, args.new_name)
@@ -217,5 +243,10 @@ write_yaml_to_file(semaphore_path, yaml_obj)
 
 print("Updating the tutorials.yml file")
 tutorials_yaml = get_yaml_file(tutorials_yaml_path)
-tutorials_yaml = update_tutorials_yaml(tutorials_yaml, new_tutorial_name, [new_tutorial_type])
+tutorials_yaml = update_tutorials_yaml(tutorials_yaml, new_tutorial_name, types_to_enable)
 write_yaml_to_file(tutorials_yaml_path, tutorials_yaml)
+print("Now updating front matter stuff")
+if args.permalink is not None:
+    update_front_matter(orig_tutorial_name, new_tutorial_name, orig_tutorial_type, new_tutorial_type, args.permalink)
+else:
+    update_front_matter(orig_tutorial_name, new_tutorial_name, orig_tutorial_type, new_tutorial_type)
