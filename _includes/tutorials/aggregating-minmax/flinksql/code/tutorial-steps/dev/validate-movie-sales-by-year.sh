a47889574bdf31c08b48e9e149d3ef0b5657bf3f
Dave Troiano <dtroiano@confluent.io>
2023-03-07 13:35:21 -0500

    address PR comments

--- _includes/tutorials/aggregating-minmax/flinksql/code/tutorial-steps/dev/validate-movie-sales-by-year.sh
+++ _includes/tutorials/aggregating-minmax/flinksql/code/tutorial-steps/dev/validate-movie-sales-by-year.sh
@@ -3,7 +3,6 @@ docker exec -it schema-registry /usr/bin/kafka-avro-console-consumer \
   --from-beginning \
   --max-messages 9 \
   --timeout-ms 10000 \
-  --property schema.registry.url=http://schema-registry:8081 \
   --bootstrap-server broker:9092 \
   --property key.deserializer=org.apache.kafka.common.serialization.IntegerDeserializer \
   --property print.key=true \
