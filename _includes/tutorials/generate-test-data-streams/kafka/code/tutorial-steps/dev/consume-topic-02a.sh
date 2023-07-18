96db7deb7143e4a714f1f09fbc18270cd7d10d32
Bill Bejeck <bbejeck@gmail.com>
2023-07-18 13:32:04 -0400

    Update Kafka version of tutorial for using datagen connector

--- _includes/tutorials/generate-test-data-streams/kafka/code/tutorial-steps/dev/consume-topic-02a.sh
+++ _includes/tutorials/generate-test-data-streams/kafka/code/tutorial-steps/dev/consume-topic-02a.sh
@@ -1,4 +1,4 @@
-docker exec -i kcat kcat -b broker:9092 -t devices \
+docker exec -i kcat kcat -b broker:9092 -t fleet_mgmt_location \
             -s key=s -s value=avro -r http://schema-registry:8081        \
             -f 'Key     (%K bytes):\t%k\nPayload (%S bytes):\t%s\n--\n' \
             -C -c3 -o beginning 2>/dev/null
