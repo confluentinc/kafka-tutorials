fe21b8a6335bbd1b7a5501b10a07feaf26c373bf
Bill Bejeck <bbejeck@gmail.com>
2023-07-18 13:32:04 -0400

    Update Kafka version of tutorial for using datagen connector

--- _includes/tutorials/generate-test-data-streams/kafka/code/tutorial-steps/dev/create-connector-03.sh
+++ _includes/tutorials/generate-test-data-streams/kafka/code/tutorial-steps/dev/create-connector-03.sh
@@ -1,15 +1,13 @@
-curl -X PUT http://localhost:8083/connectors/pizza_orders/config \
+curl -X PUT http://localhost:8083/connectors/fleet_sensor/config \
      -i -H "Content-Type: application/json" -d '{
-    "connector.class"             : "io.confluent.kafka.connect.datagen.DatagenConnector",
-    "quickstart"                  : "pizza_orders",
-    "schema.keyfield"             : "store_id",
-    "kafka.topic"                 : "pizza_orders",
+     "connector.class"             : "io.confluent.kafka.connect.datagen.DatagenConnector",
+    "quickstart"                  : "fleet_mgmt_sensors",
+    "schema.keyfield"             : "vehicle_id",
+    "kafka.topic"                 : "fleet_mgmt_sensors",
     "key.converter"               : "org.apache.kafka.connect.converters.IntegerConverter",
     "value.converter"             : "io.confluent.connect.avro.AvroConverter",
     "value.converter.schema.registry.url"         : "http://schema-registry:8081",
     "value.converter.schemas.enable": "false",
-    "max.interval": 1000,
+    "max.interval": 500,
     "tasks.max": "1"
-    
 }'
-
