2c34763c424d6ccb7267842357cba1cb624ffe81
Bill Bejeck <bbejeck@gmail.com>
2023-07-21 14:32:47 -0400

    Fixing tutorial to use DatagenConnector vs. Voluable (#1600)
    * Fixing tutorial to use DatagenConnector vs. Voluable
    
    * Add new line to output files
    
    * Add copy of schema to harness file
    
    * Update Kafka version of tutorial for using datagen connector
    
    * Correct the create connector page in harness file
    
    * Another harness file fix
    
    * Add kafka test to semaphore
    
    * Restore PR deployments
    
    * Add pulling ruby dependencies to PR deploy
    
    * Changes for added DatagenConnector
    
    * Fix harness file for jekyll build
    
    * Addressing comments on PR

--- _includes/tutorials/generate-test-data-streams/ksql/code/tutorial-steps/dev/create-connector-03.sql
+++ _includes/tutorials/generate-test-data-streams/ksql/code/tutorial-steps/dev/create-connector-03.sql
@@ -1,12 +1,12 @@
-CREATE SOURCE CONNECTOR IF NOT EXISTS PIZZA_ORDERS WITH (
+CREATE SOURCE CONNECTOR IF NOT EXISTS FLEET_SENSORS WITH (
     'connector.class'                             = 'io.confluent.kafka.connect.datagen.DatagenConnector',
-    'quickstart'                                  = 'pizza_orders',
-    'schema.keyfield'                             = 'store_id',
-    'kafka.topic'                                 = 'pizza_orders',
+    'quickstart'                                  = 'fleet_mgmt_sensors',
+    'schema.keyfield'                             = 'vehicle_id',
+    'kafka.topic'                                 = 'fleet_mgmt_sensors',
     'key.converter'                               = 'org.apache.kafka.connect.converters.IntegerConverter',
     'value.converter'                             = 'io.confluent.connect.avro.AvroConverter',
     'value.converter.schema.registry.url'         = 'http://schema-registry:8081',
     'value.converter.schemas.enable'              = 'false',
-    'max.interval'                                = 1000,
-    'tasks.max'                                   = 1
+    'max.interval'                                = 500,
+    'tasks.max'                                   = '1'
 );
