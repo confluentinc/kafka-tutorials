5bc1ffb11434ea87a5722404737ae3aebfcd378e
Brian Strauch <bstrauch@confluent.io>
2023-08-04 14:12:47 -0700

    use no-authentication and switch aggregating-minmax demo

--- _includes/tutorials/aggregating-count/flinksql/code/tutorial-steps/dev/set-topic-cleanup-policy.sh
+++ _includes/tutorials/aggregating-count/flinksql/code/tutorial-steps/dev/set-topic-cleanup-policy.sh
@@ -1,4 +1,4 @@
 docker exec -it confluent-cli confluent kafka topic update movie-ticket-sales-by-title \
   --config cleanup.policy=compact \
   --url http://rest-proxy:8082 \
-  --no-auth
+  --no-authentication
