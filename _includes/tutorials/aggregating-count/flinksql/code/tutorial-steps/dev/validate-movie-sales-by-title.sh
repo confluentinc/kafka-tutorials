64f5a684017e7f9463d9bec1a5a5d7b100742284
Brian Strauch <bstrauch@confluent.io>
2023-08-04 15:19:32 -0700

    use integer deserialization for consumer example

--- _includes/tutorials/aggregating-count/flinksql/code/tutorial-steps/dev/validate-movie-sales-by-title.sh
+++ _includes/tutorials/aggregating-count/flinksql/code/tutorial-steps/dev/validate-movie-sales-by-title.sh
@@ -1,8 +1,8 @@
 docker exec -it confluent-cli confluent kafka topic consume movie-ticket-sales-by-title \
+  --bootstrap broker:9092 \
+  --protocol PLAINTEXT \
   --from-beginning \
   --print-key \
   --delimiter "-" \
-  --bootstrap broker:9092 \
-  --protocol PLAINTEXT \
-  --schema-registry-endpoint http://schema-registry:8081 \
-  --value-format avro
+  --value-format avro \
+  --schema-registry-endpoint http://schema-registry:8081
--- _includes/tutorials/aggregating-minmax/flinksql/code/tutorial-steps/dev/validate-movie-sales-by-year.sh
+++ _includes/tutorials/aggregating-minmax/flinksql/code/tutorial-steps/dev/validate-movie-sales-by-year.sh
@@ -1,9 +1,9 @@
-docker exec -e SCHEMA_REGISTRY_LOG4J_OPTS=" " -it schema-registry /usr/bin/kafka-avro-console-consumer \
-  --topic movie-sales-by-year \
+docker exec -it confluent-cli confluent kafka topic consume movie-sales-by-year \
+  --bootstrap broker:9092 \
+  --protocol PLAINTEXT \
   --from-beginning \
-  --max-messages 9 \
-  --timeout-ms 10000 \
-  --bootstrap-server broker:9092 \
-  --property key.deserializer=org.apache.kafka.common.serialization.IntegerDeserializer \
-  --property print.key=true \
-  --property key.separator="-"
+  --print-key \
+  --key-format integer \
+  --delimiter "-" \
+  --value-format avro \
+  --schema-registry-endpoint http://schema-registry:8081
