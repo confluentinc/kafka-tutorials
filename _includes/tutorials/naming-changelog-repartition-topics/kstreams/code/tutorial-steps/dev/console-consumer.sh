851ccfeae19c59d9937b33209bc7e6f6ff42488c
Bill Bejeck <bbejeck@gmail.com>
2023-08-22 11:20:41 -0400

    Fixed tutorial output for console consumer close, updated version, added git ignore for testing output

--- _includes/tutorials/naming-changelog-repartition-topics/kstreams/code/tutorial-steps/dev/console-consumer.sh
+++ _includes/tutorials/naming-changelog-repartition-topics/kstreams/code/tutorial-steps/dev/console-consumer.sh
@@ -1,4 +1,5 @@
 docker exec -it broker /usr/bin/kafka-console-consumer --topic output-topic --bootstrap-server broker:9092 \
  --from-beginning \
  --property print.key=true \
- --property key.separator="-"
+ --property key.separator="-" \
+ --max-messages 12
