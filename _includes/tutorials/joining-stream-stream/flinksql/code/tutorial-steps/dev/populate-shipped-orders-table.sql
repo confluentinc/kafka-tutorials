a148b77e16553aa8e68915527423c3d8a8552d7c
Bill Bejeck <bbejeck@gmail.com>
2023-08-28 18:05:26 -0400

    Complete tutorial section and adding test section

---
+++ _includes/tutorials/joining-stream-stream/flinksql/code/tutorial-steps/dev/populate-shipped-orders-table.sql
@@ -0,0 +1,18 @@
+INSERT INTO shipped_orders
+
+SELECT o.id as order_id,
+           FROM_UNIXTIME(o.order_ts_raw) as ORDER_TS,
+           o.total_amount as TOTAL,
+           o.customer_name as CUSTOMER,
+           s.id as SHIP_ID,
+           FROM_UNIXTIME(s.ship_ts_raw) as SHIP_TS,
+           s.warehouse,
+           TIMESTAMPDIFF(HOUR,
+             TO_TIMESTAMP(FROM_UNIXTIME(o.order_ts_raw)),
+             TO_TIMESTAMP(FROM_UNIXTIME(s.ship_ts_raw))) as HR_TO_SHIP   
+    FROM orders o inner join shipments s
+    ON o.id = s.order_id
+    AND TO_TIMESTAMP(FROM_UNIXTIME(s.ship_ts_raw)) 
+     BETWEEN TO_TIMESTAMP(FROM_UNIXTIME(o.order_ts_raw)) 
+     AND TO_TIMESTAMP(FROM_UNIXTIME(o.order_ts_raw))  + INTERVAL '7' DAY;
+
