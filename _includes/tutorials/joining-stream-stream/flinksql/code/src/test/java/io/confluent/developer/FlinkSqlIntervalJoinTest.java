4df0ef993bab3a976f134358ff932187cf77d623
Bill Bejeck <bbejeck@gmail.com>
2023-08-29 15:00:17 -0400

    Tutorial rendering from end-to-end

--- _includes/tutorials/joining-stream-stream/flinksql/code/src/test/java/io/confluent/developer/FlinkSqlIntervalJoinTest.java
+++ _includes/tutorials/joining-stream-stream/flinksql/code/src/test/java/io/confluent/developer/FlinkSqlIntervalJoinTest.java
@@ -13,24 +13,27 @@ public class FlinkSqlIntervalJoinTest extends AbstractFlinkKafkaTest {
   @Test
   public void simpleSelect() throws Exception {
     // create base movie sales table and aggregation table, and populate with test data
-    streamTableEnv.executeSql(getResourceFileContents("create-movie-sales.sql.template",
-        Optional.of(kafkaPort), Optional.of(schemaRegistryPort))).await();
-    streamTableEnv.executeSql(getResourceFileContents("populate-movie-sales.sql"));
-    streamTableEnv.executeSql(getResourceFileContents("create-movie-sales-by-year.sql.template",
-        Optional.of(kafkaPort), Optional.of(schemaRegistryPort))).await();
+    streamTableEnv.executeSql(getResourceFileContents("create-orders.sql.template",
+        Optional.of(kafkaPort),Optional.of(schemaRegistryPort))).await();
+    streamTableEnv.executeSql(getResourceFileContents("create-shipments.sql.template",
+        Optional.of(kafkaPort),Optional.of(schemaRegistryPort))).await();
+    streamTableEnv.executeSql(getResourceFileContents("populate-orders.sql"));
+    streamTableEnv.executeSql(getResourceFileContents("populate-shipments.sql"));
+    streamTableEnv.executeSql(getResourceFileContents("create-shipped-orders.sql.template",
+        Optional.of(kafkaPort),Optional.of(schemaRegistryPort))).await();
 
-    // We can't call await() on this result since it won't return. In Flink 17 and later this can change
-    // by setting 'scan.bounded.mode' = 'latest-offset' in the movie_sales CREATE TABLE statement, which will
-    // cause this INSERT to terminate once the latest offset of movie_sales table is reached.
-    streamTableEnv.executeSql(getResourceFileContents("populate-movie-sales-by-year.sql"));
+    // In Flink 17 and later
+    // by setting 'scan.bounded.mode' = 'latest-offset' the CREATE TABLE statement, will
+    // cause this INSERT to terminate once the latest offset is reached.
+    streamTableEnv.executeSql(getResourceFileContents("populate-shipped-orders-table.sql"));
 
     // execute query on result table that should have movie sales aggregated by release year
-    TableResult tableResult = streamTableEnv.executeSql(getResourceFileContents("query-movie-sales-by-year.sql"));
+    TableResult tableResult = streamTableEnv.executeSql(getResourceFileContents("query-join-order-shipments.sql"));
 
     // Compare actual and expected results. Convert result output to line sets to compare so that order
-    // doesn't matter, because the grouped result order doesn't matter -- 2017's could come before or after 2019's.
+    // doesn't matter
     String actualTableauResults = tableauResults(tableResult);
-    String expectedTableauResults = getResourceFileContents("expected-movie-sales-by-year.txt");
+    String expectedTableauResults = getResourceFileContents("expected-shipped-orders.txt");
     assertEquals(stringToLineSet(actualTableauResults), stringToLineSet(expectedTableauResults));
   }
 
--- _includes/tutorials/joining-stream-stream/flinksql/code/src/test/resources/query-join-order-shipments.sql
+++ _includes/tutorials/joining-stream-stream/flinksql/code/src/test/resources/query-join-order-shipments.sql
@@ -1,11 +1,1 @@
-select o.id as order_id,
-           o.order_ts_ltz,
-           o.total_amount,
-           o.customer_name,
-           s.id as shipment_id,
-           s.ship_ts_ltz,
-           s.warehouse,
-           (s.ship_ts - o.order_ts) / 1000 / 60 as shipment_time      
-    from orders o inner join shipments s
-    on o.id = s.order_id
-    and o.order_ts_ltz between o.order_ts_ltz - INTERVAL '1' DAY and s.ship_ts_ltz;
+SELECT * FROM shipped_orders;
