9f1cb6d8d0ae9571e9d9fa1c8406d0ebb46dcf77
Dave Troiano <dtroiano@confluent.io>
2023-09-14 11:38:55 -0400

    fix failing Flink SQL splitting test

--- _includes/tutorials/splitting/flinksql/code/tutorial-steps/dev/validate-acting-events-fantasy.sh
+++ _includes/tutorials/splitting/flinksql/code/tutorial-steps/dev/validate-acting-events-fantasy.sh
@@ -1,7 +1,7 @@
 docker exec -e SCHEMA_REGISTRY_LOG4J_OPTS=" " -it schema-registry /usr/bin/kafka-avro-console-consumer \
   --topic acting-events-fantasy \
   --from-beginning \
-  --max-messages 10 \
+  --max-messages 4 \
   --timeout-ms 10000 \
   --bootstrap-server broker:9092 \
 
