b16024ab2d4c54074e72ee471b0a877ad572b9d0
Bill Bejeck <bbejeck@gmail.com>
2023-09-20 13:28:23 -0400

    Completed tutorial

--- _includes/tutorials/finding-distinct/flinksql/code/src/test/java/io/confluent/developer/FlinkSqlFindingDistinctTest.java
+++ _includes/tutorials/finding-distinct/flinksql/code/src/test/java/io/confluent/developer/FlinkSqlFindingDistinctTest.java
@@ -5,36 +5,32 @@ import org.apache.flink.table.api.TableResult;
 import org.junit.Test;
 
 import java.util.Optional;
-import java.util.Set;
 
 import static org.junit.Assert.*;
 
-public class FlinkSqlIntervalJoinTest extends AbstractFlinkKafkaTest {
+public class FlinkSqlFindingDistinctTest extends AbstractFlinkKafkaTest {
 
   @Test
   public void simpleSelect() throws Exception {
     // create base movie sales table and aggregation table, and populate with test data
-    streamTableEnv.executeSql(getResourceFileContents("create-orders.sql.template",
+    streamTableEnv.executeSql(getResourceFileContents("create-clicks-table.sql.template",
         Optional.of(kafkaPort),Optional.of(schemaRegistryPort))).await();
-    streamTableEnv.executeSql(getResourceFileContents("create-shipments.sql.template",
+    streamTableEnv.executeSql(getResourceFileContents("create-deduplicated-clicks-table.sql.template",
         Optional.of(kafkaPort),Optional.of(schemaRegistryPort))).await();
-    streamTableEnv.executeSql(getResourceFileContents("populate-orders.sql"));
-    streamTableEnv.executeSql(getResourceFileContents("populate-shipments.sql"));
-    streamTableEnv.executeSql(getResourceFileContents("create-shipped-orders.sql.template",
-        Optional.of(kafkaPort),Optional.of(schemaRegistryPort))).await();
+    streamTableEnv.executeSql(getResourceFileContents("populate-clicks.sql"));
 
     // In Flink 17 and later
     // by setting 'scan.bounded.mode' = 'latest-offset' the CREATE TABLE statement, will
     // cause this INSERT to terminate once the latest offset is reached.
-    streamTableEnv.executeSql(getResourceFileContents("populate-shipped-orders-table.sql"));
+    streamTableEnv.executeSql(getResourceFileContents("populate-deduplicated-clicks-table.sql"));
 
     // execute query on result table that should have joined shipments with orders
-    TableResult tableResult = streamTableEnv.executeSql(getResourceFileContents("query-join-order-shipments.sql"));
+    TableResult tableResult = streamTableEnv.executeSql(getResourceFileContents("query-deduplicated-clicks.sql"));
 
     // Compare actual and expected results. Convert result output to line sets to compare so that order
     // doesn't matter
     String actualTableauResults = tableauResults(tableResult);
-    String expectedTableauResults = getResourceFileContents("expected-shipped-orders.txt");
+    String expectedTableauResults = getResourceFileContents("expected-deduplicated-clicks.txt");
     assertEquals(stringToLineSet(actualTableauResults), stringToLineSet(expectedTableauResults));
   }
 
